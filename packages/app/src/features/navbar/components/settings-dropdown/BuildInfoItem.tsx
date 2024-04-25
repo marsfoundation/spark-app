667052bfe1f86ba6e4c13238f71be58476c5b44a
yivlad <jacenko.vlad@gmail.com>
2024-04-25 14:05:01 +0200

    Add link to github in navbar settings menu

--- packages/app/src/features/navbar/components/settings-dropdown/BuildInfoItem.tsx
+++ packages/app/src/features/navbar/components/settings-dropdown/BuildInfoItem.tsx
@@ -1,3 +1,5 @@
+import { LinkDecorator } from '@/ui/atoms/link-decorator/LinkDecorator'
+
 import { SettingsDropdownItem } from './SettingsDropdownItem'
 
 export function BuildInfoItem() {
@@ -5,10 +7,12 @@ export function BuildInfoItem() {
   const buildTime = import.meta.env.STORYBOOK_PREVIEW || __BUILD_TIME__ === undefined ? 'n/a' : __BUILD_TIME__
 
   return (
-    <SettingsDropdownItem variant="footnote">
-      <div className="text-basics-dark-grey fill-basics-dark-grey flex flex-row items-center gap-1 text-[9px]">
-        Built from {buildSha} at {buildTime}
-      </div>
-    </SettingsDropdownItem>
+    <LinkDecorator to="https://github.com/marsfoundation/spark-app" external>
+      <SettingsDropdownItem variant="footnote">
+        <div className="text-basics-dark-grey fill-basics-dark-grey flex flex-row items-center gap-1 text-[9px]">
+          Built from {buildSha} at {buildTime}
+        </div>
+      </SettingsDropdownItem>
+    </LinkDecorator>
   )
 }
