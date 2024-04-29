7ea336e19fdc8c6b56f53f4f150618c7411b09bb
Oskar <oskar.vu@proton.me>
2024-04-29 11:55:29 +0200

    Add safe injected wallet to wagmi config

---
+++ packages/app/src/config/wagmi/getWallets.ts
@@ -0,0 +1,25 @@
+import { getDefaultWallets, WalletList } from '@rainbow-me/rainbowkit'
+import {
+  coinbaseWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  safeWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets'
+
+const { wallets } = getDefaultWallets()
+const popularWallets = wallets[0]
+
+export function getWallets(): WalletList {
+  return [
+    {
+      groupName: popularWallets?.groupName ?? 'Popular',
+      wallets: [
+        // Always shown
+        ...(popularWallets?.wallets ?? [rainbowWallet, coinbaseWallet, metaMaskWallet, walletConnectWallet]),
+        // Safe wallet (shown when available)
+        safeWallet,
+      ],
+    },
+  ]
+}
