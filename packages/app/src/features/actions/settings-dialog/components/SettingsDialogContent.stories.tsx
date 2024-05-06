71d61098117cacdbe8c5cf5e4bb18004086ca40d
yivlad <jacenko.vlad@gmail.com>
2024-05-06 11:40:02 +0200

    Fix stories

--- packages/app/src/features/actions/settings-dialog/components/SettingsDialogContent.stories.tsx
+++ packages/app/src/features/actions/settings-dialog/components/SettingsDialogContent.stories.tsx
@@ -1,6 +1,9 @@
 import { WithClassname } from '@storybook/decorators'
 import { Meta, StoryObj } from '@storybook/react'
 import { getMobileStory, getTabletStory } from '@storybook/viewports'
+import { useForm } from 'react-hook-form'
+
+import { Percentage } from '@/domain/types/NumericValues'
 
 import { SettingsDialogContent } from './SettingsDialogContent'
 
@@ -8,6 +11,22 @@ const meta: Meta<typeof SettingsDialogContent> = {
   title: 'Features/Actions/SettingsDialogContent',
   component: SettingsDialogContent,
   decorators: [WithClassname('max-w-xl')],
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const form = useForm() as any
+    return (
+      <SettingsDialogContent
+        onConfirm={() => {}}
+        permitsSettings={{ preferPermits: true, togglePreferPermits: () => {} }}
+        slippageSettings={{
+          form,
+          slippage: Percentage(0.005),
+          type: 'button',
+          onSlippageChange: () => {},
+        }}
+      />
+    )
+  },
 }
 
 export default meta
