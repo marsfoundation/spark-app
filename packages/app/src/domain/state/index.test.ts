9fbf4be67c4ffefc41d659c70bd63d0e2e336f72
yivlad <jacenko.vlad@gmail.com>
2024-05-06 12:21:01 +0200

    Lint

--- packages/app/src/domain/state/index.test.ts
+++ packages/app/src/domain/state/index.test.ts
@@ -1,5 +1,5 @@
-import { create } from 'zustand'
 import { expect } from 'vitest'
+import { create } from 'zustand'
 
 import { ZUSTAND_APP_STORE_LOCAL_STORAGE_KEY } from '@/config/consts'
 import { makeFunctionsComparisonStable } from '@/test/integration/object-utils'
