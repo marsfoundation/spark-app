cd68f43999bfd75c950aea2a929fae4618746cd3
yivlad <jacenko.vlad@gmail.com>
2024-05-08 16:05:00 +0200

    Replace tooltips with popovers on touch screens

--- packages/app/src/ui/atoms/tooltip/Tooltip.tsx
+++ packages/app/src/ui/atoms/tooltip/Tooltip.tsx
@@ -1,13 +1,15 @@
+import * as PopoverPrimitive from '@radix-ui/react-popover'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 import * as React from 'react'
 
 import { cn } from '@/ui/utils/style'
 
 const TooltipProvider = TooltipPrimitive.Provider
+const RadixPrimitive = isTouchScreen() ? PopoverPrimitive : TooltipPrimitive
 
-const Tooltip = TooltipPrimitive.Root
+const Tooltip = RadixPrimitive.Root
 
-const TooltipTrigger = TooltipPrimitive.Trigger
+const TooltipTrigger = RadixPrimitive.Trigger
 
 const baseTooltipContentClassList = cn(
   'z-50 overflow-hidden',
@@ -22,11 +24,11 @@ const tooltipContentShortClassList = cn(
 const tooltipContentLongClassList = cn(baseTooltipContentClassList, 'px-5 py-4')
 
 const TooltipContentShort = React.forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+  React.ElementRef<typeof RadixPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof RadixPrimitive.Content>
 >(({ className, sideOffset = 4, children, ...props }, ref) => (
-  <TooltipPrimitive.Portal>
-    <TooltipPrimitive.Content
+  <RadixPrimitive.Portal>
+    <RadixPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
       className={cn(tooltipContentShortClassList, className)}
@@ -34,18 +36,18 @@ const TooltipContentShort = React.forwardRef<
       {...props}
     >
       {children}
-      <TooltipPrimitive.Arrow width={16} height={8} fill="white" />
-    </TooltipPrimitive.Content>
-  </TooltipPrimitive.Portal>
+      <RadixPrimitive.Arrow width={16} height={8} fill="white" />
+    </RadixPrimitive.Content>
+  </RadixPrimitive.Portal>
 ))
 TooltipContentShort.displayName = 'TooltipContentShort'
 
 const TooltipContentLong = React.forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+  React.ElementRef<typeof RadixPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof RadixPrimitive.Content>
 >(({ className, sideOffset = 4, children, ...props }, ref) => (
-  <TooltipPrimitive.Portal>
-    <TooltipPrimitive.Content
+  <RadixPrimitive.Portal>
+    <RadixPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
       className={cn(tooltipContentLongClassList, className)}
@@ -53,10 +55,14 @@ const TooltipContentLong = React.forwardRef<
       {...props}
     >
       {children}
-      <TooltipPrimitive.Arrow width={16} height={8} fill="white" />
-    </TooltipPrimitive.Content>
-  </TooltipPrimitive.Portal>
+      <RadixPrimitive.Arrow width={16} height={8} fill="white" />
+    </RadixPrimitive.Content>
+  </RadixPrimitive.Portal>
 ))
 TooltipContentLong.displayName = 'TooltipContentLong'
 
 export { Tooltip, TooltipContentLong, TooltipContentShort, TooltipProvider, TooltipTrigger }
+
+function isTouchScreen(): boolean {
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0
+}
