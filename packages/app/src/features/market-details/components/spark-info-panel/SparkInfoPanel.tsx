d32eb21330f926dc9b36d2a1a3f343900b038d10
yivlad <jacenko.vlad@gmail.com>
2024-05-17 12:42:29 +0200

    Lint

--- packages/app/src/features/market-details/components/spark-info-panel/SparkInfoPanel.tsx
+++ packages/app/src/features/market-details/components/spark-info-panel/SparkInfoPanel.tsx
@@ -10,7 +10,7 @@ export function SparkInfoPanel({ title, content }: SparkInfoProps) {
   return (
     <div className="bg-spark/10 col-start-1 col-end-[-1] mt-6 flex items-center rounded-lg border-none py-4">
       <div className="mx-3">
-        <img src={assets.sparkIcon} alt="Spark logo" className='h-[2.75rem]' />
+        <img src={assets.sparkIcon} alt="Spark logo" className="h-[2.75rem]" />
       </div>
       <div className="content-center">
         <Typography variant="h4" className="col-span-1 mt-1 content-end">
