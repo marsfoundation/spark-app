cd85cbec3403a9ef7a67abfb25766c4afab6014b
Kris Kaczor <krzkaczor@gmail.com>
2024-05-27 18:20:16 +0400

    Disable sentry integration on preview environments (#54)
    * Disable sentry integration on preview environments
    
    * fix
    
    * fix
    
    * fix

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -9,6 +9,11 @@ import { execSync } from 'child_process'
 const buildSha = execSync('git rev-parse --short HEAD').toString().trimEnd()
 const buildTime = new Date().toLocaleString('en-gb')
 
+// disable sentry integration on preview deployments
+if (process.env.VERCEL_ENV === 'preview') {
+  process.env.VITE_SENTRY_DSN = ''
+}
+
 export default defineConfig({
   define: {
     __BUILD_SHA__: JSON.stringify(buildSha),
