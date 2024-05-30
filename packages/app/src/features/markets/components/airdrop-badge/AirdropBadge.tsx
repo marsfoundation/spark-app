110f42ddc2fefb0fc39a5f70939391386a0076cc
Oskar <oskar.vu@proton.me>
2024-05-30 12:47:55 +0200

    Remove number of tokens from airdrop badge

--- packages/app/src/features/markets/components/airdrop-badge/AirdropBadge.tsx
+++ packages/app/src/features/markets/components/airdrop-badge/AirdropBadge.tsx
@@ -1,5 +1,3 @@
-import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
-import { USD_MOCK_TOKEN } from '@/domain/types/Token'
 import { assets } from '@/ui/assets'
 import { IconPill } from '@/ui/atoms/icon-pill/IconPill'
 import { Link } from '@/ui/atoms/link/Link'
@@ -7,12 +5,7 @@ import { Tooltip, TooltipContentLong, TooltipTrigger } from '@/ui/atoms/tooltip/
 import { TooltipContentLayout } from '@/ui/atoms/tooltip/TooltipContentLayout'
 import { links } from '@/ui/constants/links'
 
-interface AirdropBadgeProps {
-  value: NormalizedUnitNumber
-}
-
-// @todo It should take into consideration other airdrops as well, when SPK is not only one
-export function AirdropBadge({ value }: AirdropBadgeProps) {
+export function AirdropBadge() {
   return (
     <Tooltip>
       <TooltipTrigger>
@@ -22,9 +15,7 @@ export function AirdropBadge({ value }: AirdropBadgeProps) {
         <TooltipContentLayout>
           <TooltipContentLayout.Header>
             <TooltipContentLayout.Icon src={assets.sparkIcon} />
-            <TooltipContentLayout.Title>
-              Eligible for {USD_MOCK_TOKEN.format(value, { style: 'compact' })} Spark Airdrop
-            </TooltipContentLayout.Title>
+            <TooltipContentLayout.Title>Eligible for Spark Airdrop</TooltipContentLayout.Title>
           </TooltipContentLayout.Header>
 
           <TooltipContentLayout.Body>
