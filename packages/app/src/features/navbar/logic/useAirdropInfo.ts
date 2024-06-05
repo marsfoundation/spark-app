d567d931c6c4d67ba6fa2debfdd1c3948fe04cc8
Oskar <oskar.vu@proton.me>
2024-06-05 13:19:32 +0200

    Correct airdrop amount to match current timestamp

--- packages/app/src/features/navbar/logic/useAirdropInfo.ts
+++ packages/app/src/features/navbar/logic/useAirdropInfo.ts
@@ -2,23 +2,35 @@ import { useQuery } from '@tanstack/react-query'
 import { useAccount } from 'wagmi'
 
 import { CheckedAddress } from '@/domain/types/CheckedAddress'
-import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
 import { airdropInfo } from '@/domain/wallet/airdropInfo'
 
-import { AirdropInfo } from '../types'
+import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
+import { useTimestamp } from '@/utils/useTimestamp'
+import { Airdrop, AirdropInfo } from '../types'
 
 export function useAirdropInfo(): AirdropInfo {
   const { address } = useAccount()
+  const { timestamp } = useTimestamp()
 
   const result = useQuery(airdropInfo(address && CheckedAddress(address)))
 
-  const amount = result.data?.tokenReward ?? NormalizedUnitNumber(0)
+  const airdrop = result.data ? getCorrectedAirdrop(result.data, timestamp) : undefined
   const isLoading = result.isLoading
   const isError = result.isError
 
   return {
-    amount,
+    airdrop,
     isLoading,
     isError,
+  }
+}
+
+function getCorrectedAirdrop(airdrop: Airdrop, timestamp: number): Airdrop {
+  const timeElapsed = timestamp > airdrop.timestamp ? timestamp - airdrop.timestamp : 0
+  const tokensFromSnapshot = airdrop.tokenRate.multipliedBy(timeElapsed)
+  const tokenReward = NormalizedUnitNumber(airdrop.tokenReward.plus(tokensFromSnapshot))
+  return {
+    ...airdrop,
+    tokenReward,
   }
 }
