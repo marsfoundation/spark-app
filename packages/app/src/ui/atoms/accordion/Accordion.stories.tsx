6aaeb9d724c039c8024913f0c42df520f8d57020
Oskar <oskar.vu@proton.me>
2024-06-06 19:13:54 +0200

    Do not open mobile accordion stories to prevent future regressions

--- packages/app/src/ui/atoms/accordion/Accordion.stories.tsx
+++ packages/app/src/ui/atoms/accordion/Accordion.stories.tsx
@@ -56,5 +56,5 @@ export const SingleOpenable: Story = {
   play: async ({ canvasElement }) => (await within(canvasElement).findByText('Item number one')).click(),
 }
 
-export const Mobile = getMobileStory(MultipleOpenable)
-export const Tablet = getTabletStory(MultipleOpenable)
+export const Mobile: Story = { ...getMobileStory(MultipleOpenable), play: undefined }
+export const Tablet: Story = { ...getTabletStory(MultipleOpenable), play: undefined }
