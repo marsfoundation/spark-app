921c2585d3a185e42000520da48ee0675daa709c
Oskar <oskar.vu@proton.me>
2024-06-07 09:43:09 +0200

    Improve usage of timestamp hook

--- packages/app/src/features/navbar/logic/use-airdrop-info/useGrowingAirdropAmount.ts
+++ packages/app/src/features/navbar/logic/use-airdrop-info/useGrowingAirdropAmount.ts
@@ -5,7 +5,7 @@ import { adjustTokenReward } from './utils/adjustTokenReward'
 
 export function useGrowingAirdropAmount(airdrop: Airdrop, enabled: boolean): NormalizedUnitNumber {
   const { timestampInMs } = useTimestamp({
-    refreshIntervalInMs: enabled ? airdrop.refreshIntervalInMs : 0,
+    refreshIntervalInMs: enabled && airdrop.tokenRatePerSecond.gt(0) ? airdrop.refreshIntervalInMs : undefined,
   })
 
   return adjustTokenReward({
