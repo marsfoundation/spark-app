ccc39db6063553b6fbf2f2c1695cd6de01b6d774
Oskar <43062492+oskarvu@users.noreply.github.com>
2024-06-12 09:33:21 +0200

    Add hooks for PSM actions (#84)
    * Add PSM actions to wagmi config and generate code
    
    * Add psm action abi
    
    * Add helper queries
    
    * Add hooks for psm actions
    
    * Take care of difference in decimals between assets token and gem
    
    * Pass tokens decimals instead of whole tokens
    
    * Use generate psm actions abi

---
+++ packages/app/src/domain/psm-actions/useSwapAndDeposit.ts
@@ -0,0 +1,71 @@
+import { psmActionsConfig } from '@/config/contracts-generated'
+import { toBigInt } from '@/utils/bigNumber'
+import { useQueryClient } from '@tanstack/react-query'
+import { useAccount, useChainId, useConfig } from 'wagmi'
+import { useContractAddress } from '../hooks/useContractAddress'
+import { ensureConfigTypes, useWrite } from '../hooks/useWrite'
+import { allowance } from '../market-operations/allowance/query'
+import { BaseUnitNumber } from '../types/NumericValues'
+import { Token } from '../types/Token'
+import { balances } from '../wallet/balances'
+import { calculateGemConversionFactor } from './utils/calculateGemConversionFactor'
+
+export interface UseSwapAndDepositArgs {
+  gem: Token
+  assetsToken: Token
+  gemAmount: BaseUnitNumber
+  onTransactionSettled?: () => void
+  enabled?: boolean
+}
+
+// @note: Swap `gem` for `assetsToken` in the PSM and deposit in the `vault`.
+// @note: Assumes PSM swap rate between `assetsToken` and `gem` is 1:1 (No PSM fee).
+export function useSwapAndDeposit({
+  gem,
+  assetsToken,
+  gemAmount: _gemAmount,
+  onTransactionSettled,
+  enabled: _enabled = true,
+}: UseSwapAndDepositArgs): ReturnType<typeof useWrite> {
+  const client = useQueryClient()
+  const wagmiConfig = useConfig()
+  const chainId = useChainId()
+
+  const psmActions = useContractAddress(psmActionsConfig.address)
+
+  const { address: receiver } = useAccount()
+  const gemAmount = toBigInt(_gemAmount)
+  const gemConversionFactor = calculateGemConversionFactor({
+    gemDecimals: gem.decimals,
+    assetsTokenDecimals: assetsToken.decimals,
+  })
+  const assetsMinAmountOut = toBigInt(_gemAmount.multipliedBy(gemConversionFactor))
+
+  const config = ensureConfigTypes({
+    address: psmActions,
+    abi: psmActionsConfig.abi,
+    functionName: 'swapAndDeposit',
+    args: [receiver!, gemAmount, assetsMinAmountOut],
+  })
+  const enabled = _enabled && _gemAmount.gt(0) && !!receiver
+
+  return useWrite(
+    {
+      ...config,
+      enabled,
+    },
+    {
+      onTransactionSettled: async () => {
+        void client.invalidateQueries({
+          queryKey: balances({ wagmiConfig, chainId, account: receiver }).queryKey,
+        })
+        void client.invalidateQueries({
+          queryKey: allowance({ wagmiConfig, chainId, token: gem.address, account: receiver!, spender: psmActions })
+            .queryKey,
+        })
+
+        onTransactionSettled?.()
+      },
+    },
+  )
+}
---
+++ packages/app/src/domain/psm-actions/useWithdrawAndSwap.test.ts
@@ -0,0 +1,75 @@
+import { psmActionsAbi, psmActionsAddress } from '@/config/contracts-generated'
+import { BaseUnitNumber } from '@/domain/types/NumericValues'
+import { getMockToken, testAddresses } from '@/test/integration/constants'
+import { handlers } from '@/test/integration/mockTransport'
+import { setupHookRenderer } from '@/test/integration/setupHookRenderer'
+import { toBigInt } from '@/utils/bigNumber'
+import { waitFor } from '@testing-library/react'
+import { mainnet } from 'viem/chains'
+import { UseWithdrawAndSwap } from './useWithdrawAndSwap'
+
+const gem = getMockToken({ address: testAddresses.token, decimals: 6 })
+const assetsToken = getMockToken({ address: testAddresses.token2, decimals: 18 })
+const account = testAddresses.alice
+const gemAmountOut = BaseUnitNumber(10)
+
+const hookRenderer = setupHookRenderer({
+  hook: UseWithdrawAndSwap,
+  account,
+  handlers: [handlers.chainIdCall({ chainId: mainnet.id }), handlers.balanceCall({ balance: 0n, address: account })],
+  args: { gem, assetsToken, gemAmountOut },
+})
+
+describe(UseWithdrawAndSwap.name, () => {
+  it('is not enabled for guest ', async () => {
+    const { result } = hookRenderer({ account: undefined })
+
+    await waitFor(() => {
+      expect(result.current.status.kind).toBe('disabled')
+    })
+  })
+
+  it('is not enabled for 0 value', async () => {
+    const { result } = hookRenderer({ args: { gemAmountOut: BaseUnitNumber(0), gem, assetsToken } })
+
+    await waitFor(() => {
+      expect(result.current.status.kind).toBe('disabled')
+    })
+  })
+
+  it('is not enabled when explicitly disabled', async () => {
+    const { result } = hookRenderer({ args: { enabled: false, gemAmountOut, gem, assetsToken } })
+
+    await waitFor(() => {
+      expect(result.current.status.kind).toBe('disabled')
+    })
+  })
+
+  it('withdraws using psm actions', async () => {
+    const { result } = hookRenderer({
+      args: { gemAmountOut, gem, assetsToken },
+      extraHandlers: [
+        handlers.contractCall({
+          to: psmActionsAddress[mainnet.id],
+          abi: psmActionsAbi,
+          functionName: 'withdrawAndSwap',
+          args: [account, toBigInt(gemAmountOut), toBigInt(gemAmountOut.multipliedBy(1e12))],
+          from: account,
+          result: 1n,
+        }),
+        handlers.mineTransaction(),
+      ],
+    })
+
+    await waitFor(() => {
+      expect(result.current.status.kind).toBe('ready')
+    })
+    expect((result.current as any).error).toBeUndefined()
+
+    result.current.write()
+
+    await waitFor(() => {
+      expect(result.current.status.kind).toBe('success')
+    })
+  })
+})
