9034428af5bd591658e65ff4c5048d8e8c9b66aa
Oskar <oskar.vu@proton.me>
2024-06-19 16:25:19 +0200

    Merge types

--- packages/app/src/features/actions/flavours/native-sdai-withdraw/dai-from-sdai/types.ts
+++ packages/app/src/features/actions/flavours/native-sdai-withdraw/dai-from-sdai/types.ts
@@ -1,21 +1,13 @@
 import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
 import { Token } from '@/domain/types/Token'
 
-export type DaiFromSDaiWithdrawObjective =
-  | {
-      type: 'daiFromSDaiWithdraw'
-      dai: Token
-      value: NormalizedUnitNumber
-      sDai: Token
-      method: 'withdraw'
-    }
-  | {
-      type: 'daiFromSDaiWithdraw'
-      dai: Token
-      value: NormalizedUnitNumber
-      sDai: Token
-      method: 'redeem'
-    }
+export type DaiFromSDaiWithdrawObjective = {
+  type: 'daiFromSDaiWithdraw'
+  dai: Token
+  value: NormalizedUnitNumber
+  sDai: Token
+  method: 'withdraw' | 'redeem'
+}
 
 export interface DaiFromSDaiWithdrawAction {
   type: 'daiFromSDaiWithdraw'
