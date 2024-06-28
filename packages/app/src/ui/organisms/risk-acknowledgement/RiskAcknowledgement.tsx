b7bc084da687817a9921096ef327f7537c5d405f
Oskar <oskar.vu@proton.me>
2024-06-28 10:42:42 +0200

    Fix wording related to loosing collateral

--- packages/app/src/ui/organisms/risk-acknowledgement/RiskAcknowledgement.tsx
+++ packages/app/src/ui/organisms/risk-acknowledgement/RiskAcknowledgement.tsx
@@ -24,13 +24,13 @@ export function RiskAcknowledgement({ warning, onStatusChange }: RiskAcknowledge
               { style: 'auto' },
             )} ${warning.token.symbol}.`}
           {warning.type === 'liquidation-warning-borrow' &&
-            'Borrowing this amount puts you at risk of quick liquidation. You may lose all of your collateral.'}
+            'Borrowing this amount puts you at risk of quick liquidation. You may lose part of your collateral.'}
           {warning.type === 'liquidation-warning-withdraw' &&
-            'Withdrawing this amount puts you at risk of quick liquidation. You may lose all of your collateral.'}
+            'Withdrawing this amount puts you at risk of quick liquidation. You may lose part of your collateral.'}
           {warning.type === 'liquidation-warning-set-collateral' &&
-            'Disabling this asset as collateral puts you at risk of quick liquidation. You may lose all of your remaining collateral.'}
+            'Disabling this asset as collateral puts you at risk of quick liquidation. You may lose part of your remaining collateral.'}
           {warning.type === 'liquidation-warning-e-mode-off' &&
-            'Disabling E-Mode decreases health factor of your position and puts you at risk of quick liquidation. You may lose all of your collateral.'}
+            'Disabling E-Mode decreases health factor of your position and puts you at risk of quick liquidation. You may lose part of your collateral.'}
         </div>
       </Alert>
       <LabeledSwitch onCheckedChange={onStatusChange} data-testid={testIds.dialog.acknowledgeRiskSwitch}>
