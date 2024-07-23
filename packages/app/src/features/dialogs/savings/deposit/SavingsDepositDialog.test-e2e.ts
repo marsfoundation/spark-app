d7f8716b8bcab314396006ac4bf09b467a2f2fd9
oskarvu <oskar.vu.dev@gmail.com>
2024-07-23 12:53:16 +0200

    Update savings deposit and withdraw dialog tests

--- packages/app/src/features/dialogs/savings/deposit/SavingsDepositDialog.test-e2e.ts
+++ packages/app/src/features/dialogs/savings/deposit/SavingsDepositDialog.test-e2e.ts
@@ -1,34 +1,31 @@
 import { ActionsPageObject } from '@/features/actions/ActionsContainer.PageObject'
 import { SavingsPageObject } from '@/pages/Savings.PageObject'
+import { PSM_ACTIONS_DEPLOYED } from '@/test/e2e/constants'
 import { setupFork } from '@/test/e2e/forking/setupFork'
-import { LIFI_TEST_USER_PRIVATE_KEY, overrideLiFiRouteWithHAR } from '@/test/e2e/lifi'
 import { setup } from '@/test/e2e/setup'
 import { test } from '@playwright/test'
-import { gnosis, mainnet } from 'viem/chains'
+import { mainnet } from 'viem/chains'
 import { SavingsDialogPageObject } from '../common/e2e/SavingsDialog.PageObject'
 
 test.describe('Savings deposit dialog', () => {
   test.describe('Mainnet', () => {
-    const blockNumber = 20025569n
-    const fork = setupFork({ blockNumber, chainId: mainnet.id })
+    const fork = setupFork({
+      blockNumber: PSM_ACTIONS_DEPLOYED,
+      chainId: mainnet.id,
+      useTenderlyVnet: true,
+    })
 
     test('can switch between tokens', async ({ page }) => {
       await setup(page, fork, {
         initialPage: 'savings',
         account: {
-          type: 'connected-pkey',
+          type: 'connected-random',
           assetBalances: {
             ETH: 1,
             DAI: 100,
             USDC: 100,
-            USDT: 100,
           },
-          privateKey: LIFI_TEST_USER_PRIVATE_KEY,
         },
-      })
-      await overrideLiFiRouteWithHAR({
-        page,
-        key: 'mainnet-deposit-switch-tokens',
       })
 
       const savingsPage = new SavingsPageObject(page)
@@ -53,14 +50,6 @@ test.describe('Savings deposit dialog', () => {
         { type: 'usdcToSDaiDeposit', asset: 'USDC' },
       ])
 
-      await depositDialog.selectAssetAction('USDT')
-      await depositDialog.fillAmountAction(100)
-      await actionsContainer.expectEnabledActionAtIndex(0)
-      await actionsContainer.expectActions([
-        { type: 'approve', asset: 'USDT' },
-        { type: 'exchange', inputAsset: 'USDT', outputAsset: 'sDAI' },
-      ])
-
       await depositDialog.selectAssetAction('DAI')
       await depositDialog.fillAmountAction(100)
       await actionsContainer.expectEnabledActionAtIndex(0)
@@ -68,66 +57,6 @@ test.describe('Savings deposit dialog', () => {
         { type: 'approve', asset: 'DAI' },
         { type: 'daiToSDaiDeposit', asset: 'DAI' },
       ])
-    })
-  })
-
-  test.describe('Gnosis', () => {
-    const blockNumber = 34309540n
-    const fork = setupFork({
-      blockNumber,
-      chainId: gnosis.id,
-      simulationDateOverride: new Date('2024-06-19T10:21:19Z'),
-    })
-
-    test('can switch between tokens', async ({ page }) => {
-      await setup(page, fork, {
-        initialPage: 'savings',
-        account: {
-          type: 'connected-pkey',
-          assetBalances: {
-            XDAI: 200,
-            USDC: 100,
-            USDT: 100,
-          },
-          privateKey: LIFI_TEST_USER_PRIVATE_KEY,
-        },
-      })
-      await overrideLiFiRouteWithHAR({
-        page,
-        key: 'gnosis-deposit-switch-tokens',
-      })
-
-      const savingsPage = new SavingsPageObject(page)
-
-      await savingsPage.clickDepositButtonAction('XDAI')
-
-      const depositDialog = new SavingsDialogPageObject({ page, type: 'deposit' })
-      const actionsContainer = new ActionsPageObject(depositDialog.locatePanelByHeader('Actions'))
-
-      await depositDialog.fillAmountAction(100)
-      await actionsContainer.expectEnabledActionAtIndex(0)
-      await actionsContainer.expectActions([{ type: 'xDaiToSDaiDeposit', asset: 'XDAI' }])
-
-      await depositDialog.selectAssetAction('USDC')
-      await depositDialog.fillAmountAction(100)
-      await actionsContainer.expectEnabledActionAtIndex(0)
-      await actionsContainer.expectActions([
-        { type: 'approve', asset: 'USDC' },
-        { type: 'exchange', inputAsset: 'USDC', outputAsset: 'sDAI' },
-      ])
-
-      await depositDialog.selectAssetAction('USDT')
-      await depositDialog.fillAmountAction(100)
-      await actionsContainer.expectEnabledActionAtIndex(0)
-      await actionsContainer.expectActions([
-        { type: 'approve', asset: 'USDT' },
-        { type: 'exchange', inputAsset: 'USDT', outputAsset: 'sDAI' },
-      ])
-
-      await depositDialog.selectAssetAction('XDAI')
-      await depositDialog.fillAmountAction(100)
-      await actionsContainer.expectEnabledActionAtIndex(0)
-      await actionsContainer.expectActions([{ type: 'xDaiToSDaiDeposit', asset: 'XDAI' }])
     })
   })
 })
