e03b63e2b23633f601289bdc97da7ec0fb642201
oskarvu <oskar.vu.dev@gmail.com>
2024-07-30 16:50:58 +0200

    Display new savings page only in dev mode

--- packages/app/src/pages/Savings.tsx
+++ packages/app/src/pages/Savings.tsx
@@ -1,12 +1,15 @@
+import { NST_DEV_CHAIN_ID } from '@/config/chain/constants'
 import { SavingsContainer as SavingsContainerWithNst } from '@/features/savings-with-nst/SavingsContainer'
 import { SavingsContainer } from '@/features/savings/SavingsContainer'
+import { useChainId } from 'wagmi'
 
 export function Savings() {
-  if (
-    typeof import.meta.env.VITE_DEV_NST_NETWORK_RPC_URL === 'string' &&
-    import.meta.env.VITE_FEATURE_SAVINGS_PAGE_WITH_NST === '1'
-  ) {
-    return <SavingsContainerWithNst />
+  if (typeof import.meta.env.VITE_DEV_NST_NETWORK_RPC_URL === 'string') {
+    // biome-ignore lint/correctness/useHookAtTopLevel: temporary solution while developing nst
+    const chainId = useChainId()
+    if (chainId === NST_DEV_CHAIN_ID) {
+      return <SavingsContainerWithNst />
+    }
   }
 
   return <SavingsContainer />
