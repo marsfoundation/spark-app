ffdf8c897fb4e036d1f774a5c159fabb8ba3b054
oskarvu <oskar.vu.dev@gmail.com>
2024-08-07 12:24:21 +0200

    Fix repay stories

--- packages/app/src/features/dialogs/repay/views/RepayView.stories.tsx
+++ packages/app/src/features/dialogs/repay/views/RepayView.stories.tsx
@@ -1,12 +1,13 @@
+import { CheckedAddress } from '@/domain/types/CheckedAddress'
+import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
+import { getMockReserve } from '@/test/integration/constants'
 import { WithClassname, WithTooltipProvider, ZeroAllowanceWagmiDecorator } from '@storybook/decorators'
 import { Meta, StoryObj } from '@storybook/react'
 import { tokens } from '@storybook/tokens'
 import { getMobileStory, getTabletStory } from '@storybook/viewports'
 import BigNumber from 'bignumber.js'
 import { useForm } from 'react-hook-form'
-
-import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
-
+import { zeroAddress } from 'viem'
 import { RepayView } from './RepayView'
 
 const meta: Meta<typeof RepayView> = {
@@ -37,7 +38,16 @@ const meta: Meta<typeof RepayView> = {
       maxValue: NormalizedUnitNumber(5000),
       changeAsset: () => {},
     },
-    objectives: [],
+    objectives: [
+      {
+        type: 'repay',
+        reserve: getMockReserve({ token: tokens.DAI }),
+        lendingPool: CheckedAddress(zeroAddress),
+        useAToken: false,
+        value: NormalizedUnitNumber(2000),
+        requiredApproval: NormalizedUnitNumber(2000),
+      },
+    ],
     pageStatus: {
       state: 'form',
       actionsEnabled: true,
