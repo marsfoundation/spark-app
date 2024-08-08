38667d204560b30f17ba80674460948e08c954c6
yivlad <jacenko.vlad@gmail.com>
2024-08-08 12:51:17 +0200

    Add tests template
    Co-authored-by: Oskar <oskarvu@users.noreply.github.com>

---
+++ packages/app/src/features/actions/flavours/approve/logic/approve-action.test.ts
@@ -0,0 +1,121 @@
+import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
+import { TokenSymbol } from '@/domain/types/TokenSymbol'
+import { getMockToken, testAddresses } from '@/test/integration/constants'
+import { expectToStay } from '@/test/integration/expect'
+import { handlers } from '@/test/integration/mockTransport'
+import { createUpdatableHandler } from '@/test/integration/mockTransport/handlers'
+import { setupUseActionRenderer } from '@/test/integration/setupUseActionRenderer'
+import { toBigInt } from '@/utils/bigNumber'
+import { waitFor } from '@testing-library/react'
+import { erc20Abi } from 'viem'
+import { mainnet } from 'viem/chains'
+import { describe } from 'vitest'
+import { createApproveActionConfig } from './approve-action'
+
+const defaultValue = NormalizedUnitNumber(1)
+const token = getMockToken({ symbol: TokenSymbol('TEST') })
+const account = testAddresses.alice
+const spender = testAddresses.bob
+const chainId = mainnet.id
+
+const hookRenderer = setupUseActionRenderer({
+  account,
+  handlers: [handlers.chainIdCall({ chainId }), handlers.balanceCall({ balance: 0n, address: account })],
+  args: { action: { type: 'approve', token, spender, value: defaultValue }, enabled: true },
+})
+
+describe(createApproveActionConfig.name, () => {
+  it('performs action', async () => {
+    const { handler: allowanceHandler, update: updateAllowanceHandler } = createUpdatableHandler({
+      initialHandler: handlers.contractCall({
+        to: token.address,
+        abi: erc20Abi,
+        functionName: 'allowance',
+        args: [account, spender],
+        result: 0n,
+      }),
+    })
+
+    const { result } = hookRenderer({
+      args: { action: { type: 'approve', token, spender, value: defaultValue }, enabled: true },
+      extraHandlers: [
+        allowanceHandler,
+        handlers.contractCall({
+          to: token.address,
+          abi: erc20Abi,
+          functionName: 'approve',
+          args: [spender, toBigInt(token.toBaseUnit(defaultValue))],
+          result: true,
+          from: account,
+        }),
+        handlers.mineTransaction(),
+      ],
+    })
+
+    await waitFor(() => {
+      expect(result.current.state.status).toBe('ready')
+    })
+
+    updateAllowanceHandler(
+      handlers.contractCall({
+        to: token.address,
+        abi: erc20Abi,
+        functionName: 'allowance',
+        args: [account, spender],
+        result: toBigInt(token.toBaseUnit(defaultValue)),
+      }),
+    )
+
+    result.current.onAction()
+
+    await waitFor(() => {
+      expect(result.current.state.status).toBe('success')
+    })
+  })
+
+  it('stays in ready state if allowance is not enough after action', async () => {
+    const { handler: allowanceHandler, update: updateAllowanceHandler } = createUpdatableHandler({
+      initialHandler: handlers.contractCall({
+        to: token.address,
+        abi: erc20Abi,
+        functionName: 'allowance',
+        args: [account, spender],
+        result: 0n,
+      }),
+    })
+
+    const { result } = hookRenderer({
+      args: { action: { type: 'approve', token, spender, value: defaultValue }, enabled: true },
+      extraHandlers: [
+        allowanceHandler,
+        handlers.contractCall({
+          to: token.address,
+          abi: erc20Abi,
+          functionName: 'approve',
+          args: [spender, toBigInt(token.toBaseUnit(defaultValue))],
+          result: true,
+          from: account,
+        }),
+        handlers.mineTransaction(),
+      ],
+    })
+
+    await waitFor(() => {
+      expect(result.current.state.status).toBe('ready')
+    })
+
+    updateAllowanceHandler(
+      handlers.contractCall({
+        to: token.address,
+        abi: erc20Abi,
+        functionName: 'allowance',
+        args: [account, spender],
+        result: toBigInt(token.toBaseUnit(defaultValue)) - 1n, // allowance is not enough
+      }),
+    )
+
+    result.current.onAction()
+
+    await expectToStay(() => result.current.state.status, 'ready')
+  })
+})
