e6728874de8822a4c3f17955fa0c558598b9cb58
oskarvu <oskar.vu.dev@gmail.com>
2024-08-08 11:12:32 +0200

    Play around more

--- packages/app/src/domain/hooks/useValueChangeCallback.ts
+++ packages/app/src/domain/hooks/useValueChangeCallback.ts
@@ -1,12 +1,8 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
-export function useValueChangeCallback<T>(value: T, callback: (newValue: T, oldValue: T | undefined) => void): void {
-  const previousValueRef = useRef(value)
-
+export function useValueChangeCallback(txReceipt: any, callback: () => void): void {
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    if (value !== previousValueRef.current) {
-      callback(value, previousValueRef.current)
-      previousValueRef.current = value
-    }
-  }, [value, callback])
+    callback()
+  }, [txReceipt])
 }
