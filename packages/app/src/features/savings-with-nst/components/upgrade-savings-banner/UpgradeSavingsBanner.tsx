98417c9a79add6f03fbf756b8a3ad22e5ddeadbd
yivlad <jacenko.vlad@gmail.com>
2024-08-16 10:32:06 +0200

    Update styles

--- packages/app/src/features/savings-with-nst/components/upgrade-savings-banner/UpgradeSavingsBanner.tsx
+++ packages/app/src/features/savings-with-nst/components/upgrade-savings-banner/UpgradeSavingsBanner.tsx
@@ -12,32 +12,39 @@ export function UpgradeSavingsBanner({ onUpgradeSavingsClick }: UpgradeSavingsBa
       className="flex min-h-[240px] w-full flex-1 flex-col justify-between gap-6 self-stretch px-6 py-6 md:gap-0 md:px-8"
       variant="blue"
     >
-      <div className="flex flex-col gap-3">
-        <h2 className="inline-block font-semibold text-base text-basics-black sm:text-xl">
-          Upgrade sDAI to sNST and get more benefits!
+      <div className="grid gap-3 md:auto-cols-max">
+        <h2 className="font-semibold text-base text-basics-black sm:text-xl">
+          Upgrade you Savings DAI (sDAI) to Savings USDS (sUSDS)
         </h2>
-        <div className="text-prompt-foreground">sNST is an upgraded stablecoin from MakerDAO's Endgame plan.</div>
+        <div>
+          <div className="grid grid-cols-subgrid text-prompt-foreground">
+            Upgrade your Savings DAI to Savings USDS and unlock the full potential of the Sky ecosystem.
+          </div>
+        </div>
       </div>
       <div className="flex w-full flex-col gap-4 md:flex-row md:justify-between md:gap-2">
         <div className="grid grid-cols-[auto_auto] gap-2">
-          <div className="col-span-full grid grid-cols-subgrid">
-            <img src={assets.success} alt="success-img" className="h-4 w-4 translate-y-1" />
-            <div>
-              Higher APY by <span className="text-product-green">0.25%</span>
-            </div>
-          </div>
-          <div className="col-span-full grid grid-cols-subgrid">
-            <img src={assets.success} alt="success-img" className="h-4 w-4 translate-y-1" />
-            <div> More investment opportunities </div>
-          </div>
+          <Benefit>
+            <span className="text-product-green">0.25% higher APY</span> compared to Savings DAI
+          </Benefit>
+          <Benefit>he upgrade is optional, and you can continue using Savings DAI</Benefit>
         </div>
         <div className="flex flex-col gap-2 md:flex-row md:items-end">
           <Button onClick={onUpgradeSavingsClick}>Upgrade now</Button>
-          <LinkButton variant="secondary" to="google.com">
+          <LinkButton variant="secondary" to="https://google.com" external>
             Learn more
           </LinkButton>
         </div>
       </div>
     </Panel.Wrapper>
+  )
+}
+
+function Benefit({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="col-span-full grid grid-cols-subgrid">
+      <img src={assets.success} alt="success-img" className="h-4 w-4 translate-y-1" />
+      <div>{children}</div>
+    </div>
   )
 }
--- packages/app/src/ui/atoms/button/Button.tsx
+++ packages/app/src/ui/atoms/button/Button.tsx
@@ -1,9 +1,9 @@
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps, cva } from 'class-variance-authority'
 import * as React from 'react'
-import { Link, LinkProps } from 'react-router-dom'
 
 import { cn } from '@/ui/utils/style'
+import { Link, LinkProps } from '../link/Link'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md border border-slate-700 border-opacity-10 font-semibold text-base ring-offset-background transition-colors disabled:pointer-events-none disabled:bg-slate-700 disabled:bg-opacity-10 disabled:text-slate-500 disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
