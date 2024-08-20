76da68d7e8364d5a413172def0d7438c6588fd96
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2024-08-20 12:08:50 +0200

    Add sDAI to NST withdraw E2E tests (#217)

--- packages/app/src/features/dialogs/savings/withdraw/e2e/nst-devnet/WithdrawMaxNST.test-e2e.ts
+++ packages/app/src/features/dialogs/savings/withdraw/e2e/nst-devnet/WithdrawMaxNST.test-e2e.ts
@@ -6,7 +6,7 @@ import { setup } from '@/test/e2e/setup'
 import { test } from '@playwright/test'
 import { SavingsDialogPageObject } from '../../../common/e2e/SavingsDialog.PageObject'
 
-test.describe('Withdraw NST on NST DevNet', () => {
+test.describe('Withdraw NST from sNST', () => {
   const fork = setupFork({ chainId: NST_DEV_CHAIN_ID })
   let savingsPage: SavingsPageObject
   let withdrawDialog: SavingsDialogPageObject
@@ -71,5 +71,73 @@ test.describe('Withdraw NST on NST DevNet', () => {
     await savingsPage.expectPotentialProjection('$40.18', '30-day')
     await savingsPage.expectPotentialProjection('$500.00', '1-year')
     await savingsPage.expectCashInWalletAssetBalance('NST', '10,000')
+  })
+})
+
+test.describe('Withdraw NST from sDAI', () => {
+  const fork = setupFork({ chainId: NST_DEV_CHAIN_ID })
+  let savingsPage: SavingsPageObject
+  let withdrawDialog: SavingsDialogPageObject
+
+  test.beforeEach(async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          ETH: 1,
+          sDAI: 10_000,
+        },
+      },
+    })
+
+    savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.clickWithdrawSDaiButtonAction()
+    withdrawDialog = new SavingsDialogPageObject({ page, type: 'withdraw' })
+    await withdrawDialog.selectAssetAction('NST')
+    await withdrawDialog.clickMaxAmountAction()
+  })
+
+  test('uses migrate sDAI to NST action', async () => {
+    await withdrawDialog.actionsContainer.expectActions([
+      { type: 'approve', asset: 'sDAI' },
+      { type: 'withdrawFromSavings', asset: 'NST', savingsAsset: 'sDAI', mode: 'withdraw' },
+    ])
+  })
+
+  test('displays transaction overview', async () => {
+    await withdrawDialog.expectNativeRouteTransactionOverview({
+      routeItems: [
+        {
+          tokenAmount: '10,000.00 sDAI',
+          tokenUsdValue: '$11,047.93',
+        },
+        {
+          tokenAmount: '11,047.93 DAI',
+          tokenUsdValue: '$11,047.93',
+        },
+        {
+          tokenAmount: '11,047.93 NST',
+          tokenUsdValue: '$11,047.93',
+        },
+      ],
+      outcome: '11,047.93 NST worth $11,047.93',
+      badgeToken: 'NST',
+    })
+
+    await withdrawDialog.expectUpgradeSwitchToBeHidden()
+  })
+
+  test('executes withdraw', async () => {
+    const actionsContainer = new ActionsPageObject(withdrawDialog.locatePanelByHeader('Actions'))
+    await actionsContainer.acceptAllActionsAction(2, fork)
+
+    await withdrawDialog.expectSuccessPage()
+    await withdrawDialog.clickBackToSavingsButton()
+
+    await savingsPage.expectCashInWalletAssetBalance('NST', '11,047.93')
+    await savingsPage.expectPotentialProjection('$44.39', '30-day')
+    await savingsPage.expectPotentialProjection('$552.40', '1-year')
   })
 })
--- packages/app/src/features/dialogs/savings/withdraw/e2e/nst-devnet/WithdrawNST.test-e2e.ts
+++ packages/app/src/features/dialogs/savings/withdraw/e2e/nst-devnet/WithdrawNST.test-e2e.ts
@@ -6,7 +6,7 @@ import { setup } from '@/test/e2e/setup'
 import { test } from '@playwright/test'
 import { SavingsDialogPageObject } from '../../../common/e2e/SavingsDialog.PageObject'
 
-test.describe('Withdraw NST on NST DevNet', () => {
+test.describe('Withdraw NST on from sNST', () => {
   const fork = setupFork({ chainId: NST_DEV_CHAIN_ID })
   let savingsPage: SavingsPageObject
   let withdrawDialog: SavingsDialogPageObject
@@ -69,6 +69,73 @@ test.describe('Withdraw NST on NST DevNet', () => {
     await withdrawDialog.clickBackToSavingsButton()
 
     await savingsPage.expectSavingsNSTBalance({ sNstBalance: '8,910.12 sNST', estimatedNstValue: '9,000' })
+    await savingsPage.expectCashInWalletAssetBalance('NST', '1,000')
+  })
+})
+
+test.describe('Withdraw NST from sDAI', () => {
+  const fork = setupFork({ chainId: NST_DEV_CHAIN_ID })
+  let savingsPage: SavingsPageObject
+  let withdrawDialog: SavingsDialogPageObject
+
+  test.beforeEach(async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          ETH: 1,
+          sDAI: 10_000,
+        },
+      },
+    })
+
+    savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.clickWithdrawSDaiButtonAction()
+    withdrawDialog = new SavingsDialogPageObject({ page, type: 'withdraw' })
+    await withdrawDialog.selectAssetAction('NST')
+    await withdrawDialog.fillAmountAction(1000)
+  })
+
+  test('uses migrate sDAI to NST action', async () => {
+    await withdrawDialog.actionsContainer.expectActions([
+      { type: 'approve', asset: 'sDAI' },
+      { type: 'withdrawFromSavings', asset: 'NST', savingsAsset: 'sDAI', mode: 'withdraw' },
+    ])
+  })
+
+  test('displays transaction overview', async () => {
+    await withdrawDialog.expectNativeRouteTransactionOverview({
+      routeItems: [
+        {
+          tokenAmount: '905.15 sDAI',
+          tokenUsdValue: '$1,000.00',
+        },
+        {
+          tokenAmount: '1,000.00 DAI',
+          tokenUsdValue: '$1,000.00',
+        },
+        {
+          tokenAmount: '1,000.00 NST',
+          tokenUsdValue: '$1,000.00',
+        },
+      ],
+      outcome: '1,000.00 NST worth $1,000.00',
+      badgeToken: 'NST',
+    })
+
+    await withdrawDialog.expectUpgradeSwitchToBeHidden()
+  })
+
+  test('executes withdraw', async () => {
+    const actionsContainer = new ActionsPageObject(withdrawDialog.locatePanelByHeader('Actions'))
+    await actionsContainer.acceptAllActionsAction(2, fork)
+
+    await withdrawDialog.expectSuccessPage()
+    await withdrawDialog.clickBackToSavingsButton()
+
+    await savingsPage.expectSavingsDAIBalance({ sDaiBalance: '9,094.85 sDAI', estimatedDaiValue: '10,047.92' })
     await savingsPage.expectCashInWalletAssetBalance('NST', '1,000')
   })
 })
