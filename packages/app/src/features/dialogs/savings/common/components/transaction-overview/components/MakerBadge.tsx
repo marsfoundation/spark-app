36952315e6b7c19c91487b1726ff35faaffe6b08
Emil Małańczak <kdddmalanczak@gmail.com>
2024-08-21 11:47:04 +0200

    Adjust maker badge inline padding

--- packages/app/src/features/dialogs/savings/common/components/transaction-overview/components/MakerBadge.tsx
+++ packages/app/src/features/dialogs/savings/common/components/transaction-overview/components/MakerBadge.tsx
@@ -11,7 +11,7 @@ export function MakerBadge({ token, 'data-testid': dataTestId }: MakerBadgeProps
   return (
     <div className="-mt-4 -z-10 relative rounded-b-lg border border-basics-border border-t-0 bg-panel-bg">
       <div
-        className="flex flex-row items-center gap-2 rounded-b-lg bg-teal-400/10 px-2.5 pt-7 pb-3 text-sm text-teal-500"
+        className="flex flex-row items-center gap-2 rounded-b-lg bg-teal-400/10 px-3 pt-7 pb-3 text-sm text-teal-500"
         data-testid={dataTestId}
       >
         <img src={assets.makerLogo} className="h-4 w-4" />
