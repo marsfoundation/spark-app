e08109e3c84b1c1029299d11acf8faf7b23d830a
yivlad <jacenko.vlad@gmail.com>
2024-08-28 17:44:40 +0200

    Bring back default margins

--- packages/app/src/features/farm-details/components/apr-over-time/defaults.ts
+++ packages/app/src/features/farm-details/components/apr-over-time/defaults.ts
@@ -1,2 +1,2 @@
 export type Margins = { top: number; right: number; bottom: number; left: number }
-export const defaultMargins: Margins = { top: 0, right: 20, bottom: 20, left: 20 }
+export const defaultMargins: Margins = { top: 40, right: 20, bottom: 20, left: 40 }
