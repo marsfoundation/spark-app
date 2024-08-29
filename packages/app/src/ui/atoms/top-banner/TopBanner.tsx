d39d97e8d8acde71ea22d5e4d47610d1f15b7ba7
Emil Małańczak <53233583+EmilMalanczak@users.noreply.github.com>
2024-08-29 12:20:44 +0200

    Update top banner announcements (#234)
    * Update top banner announcements
    
    * Add fancy gradient on TopBanner
    
    * Add icon support for SettingsDropdown

--- packages/app/src/ui/atoms/top-banner/TopBanner.tsx
+++ packages/app/src/ui/atoms/top-banner/TopBanner.tsx
@@ -2,14 +2,14 @@ import { Link } from '../link/Link'
 
 export function TopBanner() {
   return (
-    <div className="w-full bg-spark/40 p-0.5 text-center text-basics-black/70 text-sm sm:p-0 sm:text-base">
-      Welcome to the <strong>new Spark App</strong>!{' '}
-      <Link to="https://twitter.com/sparkdotfi" external className="underline">
+    <div className="w-full bg-[radial-gradient(circle_at_center,_#413A96_0%,_#352D7D_50%,_rgb(199,141,242)_100%)] p-0.5 text-center text-basics-white/85 text-sm sm:p-0 sm:text-base">
+      MakerDAO is now <strong className="text-basics-white">Sky</strong>.{' '}
+      <Link
+        to="https://forum.makerdao.com/t/sky-has-arrived/24959"
+        external
+        className="text-basics-white/90 underline hover:text-basics-white"
+      >
         Read the announcement
-      </Link>
-      . Old app is available under{' '}
-      <Link to="https://legacy-app.spark.fi/" external className="underline">
-        legacy-app.spark.fi
       </Link>
     </div>
   )
