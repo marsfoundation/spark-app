f499183e8ba25580de2d876941d558dd50be9c0c
Emil Małańczak <53233583+EmilMalanczak@users.noreply.github.com>
2024-08-29 15:33:49 +0200

    Add market details links (#235)
    * Add TokenLinksDropdown component UI
    
    * Update Market details story
    
    * Fix failing mobile viewport
    
    * Add anchors for links dropdown
    
    * Add missing storybook decorator
    
    * Extend useBlockExplorerAddressLink with optional chainId param
    
    * Switch market details link chainId

--- packages/app/src/features/market-details/views/components/Header.tsx
+++ packages/app/src/features/market-details/views/components/Header.tsx
@@ -1,19 +1,30 @@
 import { Token } from '@/domain/types/Token'
 import { Alert } from '@/features/dialogs/common/components/alert/Alert'
 import { TokenIcon } from '@/ui/atoms/token-icon/TokenIcon'
+import { Address } from 'viem'
+import { TokenLinksDropdown } from './TokenLinksDropdown'
 
 interface HeaderProps {
   token: Token
+  aToken: Token
+  variableDebtTokenAddress: Address
   chainName: string
   chainMismatch: boolean
+  chainId: number
 }
 
-export function Header({ token, chainName, chainMismatch }: HeaderProps) {
+export function Header({ token, aToken, variableDebtTokenAddress, chainName, chainMismatch, chainId }: HeaderProps) {
   return (
     <div className="mt-6 mb-4 flex flex-col gap-4 sm:mt-8 sm:mb-10">
       <div className="flex items-center gap-3 px-3 md:ml-5 lg:px-0">
         <TokenIcon token={token} className="h-8 w-8" />
         <h1 className="font-semibold text-4xl text-sky-950">{token.symbol}</h1>
+        <TokenLinksDropdown
+          token={token}
+          aToken={aToken}
+          variableDebtTokenAddress={variableDebtTokenAddress}
+          chainId={chainId}
+        />
       </div>
       {chainMismatch && (
         <Alert variant="warning">
