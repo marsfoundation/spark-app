5e1e8b66afcf108ef34186ccc5663322de0d70e3
yivlad <jacenko.vlad@gmail.com>
2024-08-30 15:59:50 +0200

    Lint

--- packages/app/src/features/savings-with-usds/components/upgrade-savings-banner/UpgradeSavingsBanner.stories.ts
+++ packages/app/src/features/savings-with-usds/components/upgrade-savings-banner/UpgradeSavingsBanner.stories.ts
@@ -2,9 +2,9 @@ import { WithTooltipProvider } from '@storybook/decorators'
 import type { Meta, StoryObj } from '@storybook/react'
 import { getMobileStory, getTabletStory } from '@storybook/viewports'
 
+import { Percentage } from '@/domain/types/NumericValues'
 import { withRouter } from 'storybook-addon-remix-react-router'
 import { UpgradeSavingsBanner } from './UpgradeSavingsBanner'
-import { Percentage } from '@/domain/types/NumericValues'
 
 const meta: Meta<typeof UpgradeSavingsBanner> = {
   title: 'Features/SavingsWithUsds/Components/UpgradeSavingsBanner',
