e2b4ab6461241d546fa684de677d60bf974e043c
oskarvu <oskar.vu.dev@gmail.com>
2024-09-03 14:23:29 +0200

    Revert changing zustand store version

--- packages/app/src/config/consts.ts
+++ packages/app/src/config/consts.ts
@@ -12,7 +12,7 @@ export const NATIVE_ASSET_MOCK_ADDRESS = CheckedAddress('0xEeeeeEeeeEeEeeEeEeEee
 export const MAX_INT = BigInt(2 ** 256 - 1)
 
 export const ZUSTAND_APP_STORE_LOCAL_STORAGE_KEY = 'zustand-app-store'
-export const ZUSTAND_APP_STORE_LOCAL_STORAGE_VERSION = 3
+export const ZUSTAND_APP_STORE_LOCAL_STORAGE_VERSION = 2
 
 export const apiUrl = import.meta.env.VITE_API_URL ?? '/api'
 export const blockAnaliticaApiUrl = import.meta.env.VITE_BLOCK_ANALITICA_API_URL ?? '/ba-api'
