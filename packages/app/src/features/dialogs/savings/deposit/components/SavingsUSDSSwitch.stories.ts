2e159b3a34d8b8209acd2ecca390172f55da93e5
oskarvu <oskar.vu.dev@gmail.com>
2024-09-03 19:43:58 +0200

    Extend deposit into susds switch stories

--- packages/app/src/features/dialogs/savings/deposit/components/SavingsUSDSSwitch.stories.ts
+++ packages/app/src/features/dialogs/savings/deposit/components/SavingsUSDSSwitch.stories.ts
@@ -1,6 +1,8 @@
+import { Percentage } from '@/domain/types/NumericValues'
 import { WithClassname } from '@storybook/decorators'
 import { Meta, StoryObj } from '@storybook/react'
 import { getMobileStory, getTabletStory } from '@storybook/viewports'
+import { within } from '@testing-library/react'
 import { SavingsUsdsSwitch } from './SavingsUsdsSwitch'
 
 const meta: Meta<typeof SavingsUsdsSwitch> = {
@@ -10,6 +12,7 @@ const meta: Meta<typeof SavingsUsdsSwitch> = {
   args: {
     checked: true,
     onSwitch: () => {},
+    apyImprovement: Percentage(0.01),
   },
 }
 
@@ -19,3 +22,16 @@ type Story = StoryObj<typeof SavingsUsdsSwitch>
 export const Desktop: Story = {}
 export const Mobile = getMobileStory(Desktop)
 export const Tablet = getTabletStory(Desktop)
+
+export const WithOpenedBenefits: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    ;(await canvas.findByRole('button')).click()
+  },
+}
+
+export const WithoutImprovedApy: Story = {
+  args: {
+    apyImprovement: undefined,
+  },
+}
