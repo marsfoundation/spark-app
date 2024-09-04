814570e1a62ea408ff302cf2a6648ebb99fabebc
oskarvu <oskar.vu.dev@gmail.com>
2024-09-04 06:58:10 +0200

    Wait for dialog to appear before taking screenshot

--- packages/app/src/features/dialogs/savings/deposit/components/SavingsUSDSSwitch.stories.ts
+++ packages/app/src/features/dialogs/savings/deposit/components/SavingsUSDSSwitch.stories.ts
@@ -1,8 +1,8 @@
 import { Percentage } from '@/domain/types/NumericValues'
 import { WithClassname } from '@storybook/decorators'
 import { Meta, StoryObj } from '@storybook/react'
+import { expect, waitFor, within } from '@storybook/test'
 import { getMobileStory, getTabletStory } from '@storybook/viewports'
-import { within } from '@testing-library/react'
 import { SavingsUsdsSwitch } from './SavingsUsdsSwitch'
 
 const meta: Meta<typeof SavingsUsdsSwitch> = {
@@ -25,8 +25,12 @@ export const Tablet = getTabletStory(Desktop)
 
 export const WithOpenedBenefits: Story = {
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
+    const canvas = within(canvasElement.ownerDocument.body)
     ;(await canvas.findByRole('button')).click()
+    const heading = await canvas.findByRole('heading', { name: 'Deposit into Savings USDS' })
+    await waitFor(async () => {
+      await expect(heading).toBeVisible()
+    })
   },
 }
 
