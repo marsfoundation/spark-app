19339f580cea3826b3d9ec4e45508ea811c1fc27
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-09 14:40:16 +0200

    Move classes around

--- packages/app/src/ui/atoms/dialog/Dialog.tsx
+++ packages/app/src/ui/atoms/dialog/Dialog.tsx
@@ -21,8 +21,9 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0',
+      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 inset-0',
       'z-50 data-[state=closed]:animate-out data-[state=open]:animate-in',
+      'fixed inset-0 grid place-items-center overflow-hidden',
       className,
     )}
     {...props}
@@ -43,7 +44,7 @@ const contentVariants = cva('', {
   },
 })
 
-const overlayVariants = cva('fixed inset-0 grid place-items-center overflow-hidden', {
+const overlayVariants = cva('', {
   variants: {
     overlayVariant: {
       moderate: 'bg-background/60 backdrop-blur-sm',
