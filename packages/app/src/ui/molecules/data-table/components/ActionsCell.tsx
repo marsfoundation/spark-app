5c160d3f380466cfce7def9610d2dc83740f110a
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-10 14:28:08 +0200

    Prevent data table action cell event propagation

--- packages/app/src/ui/molecules/data-table/components/ActionsCell.tsx
+++ packages/app/src/ui/molecules/data-table/components/ActionsCell.tsx
@@ -6,7 +6,12 @@ interface ActionsCellWrapperProps {
 
 export function ActionsCell({ children }: ActionsCellWrapperProps) {
   return (
-    <div className="mt-4 flex justify-end md:mt-0">
+    <div
+      className="mt-4 flex justify-end md:mt-0"
+      onClick={(e) => {
+        e.stopPropagation()
+      }}
+    >
       <div className="flex w-full gap-2 md:w-40">{children}</div>
     </div>
   )
