7aaf1df00eedb99f2364d355cd6c3268ddfbf8be
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-12 14:46:00 +0200

    Hide market overview for non borrowable tokens

--- packages/app/src/features/market-details/views/components/FullView.tsx
+++ packages/app/src/features/market-details/views/components/FullView.tsx
@@ -41,7 +41,9 @@ export function FullView({
           <BorrowStatusPanel token={token} {...marketOverview.borrow} />
         </div>
         <div className="flex flex-col gap-6">
-          <MarketOverviewPanel token={token} {...marketOverview.summary} />
+          {(marketOverview.borrow.status === 'no' ? !marketOverview.summary.borrowed.isZero() : true) && (
+            <MarketOverviewPanel token={token} {...marketOverview.summary} />
+          )}
           <MyWalletPanel
             openDialog={openDialog}
             walletOverview={walletOverview}
