a886c62c0df36ba52e6788f6b77b3a791791155c
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-13 14:23:57 +0200

    Fix copy

--- packages/app/src/features/easy-borrow/components/UsdsUpgradeAlert.tsx
+++ packages/app/src/features/easy-borrow/components/UsdsUpgradeAlert.tsx
@@ -16,14 +16,14 @@ export function UsdsUpgradeAlert({ borrowDetails, variant, className }: UsdsUpgr
         {variant === 'borrow' && (
           <>
             Borrowing {usds} creates a {dai} borrow position in Spark Lend. The borrowed {dai} is upgraded to {usds} in
-            a separate action listed in the actions panel below. You will see your {dai} position on the portfolio, and
+            a separate action listed in the actions panel below. You will see your {dai} position in the portfolio, and
             your new {usds} balance will be reflected on the Cash&nbsp;&&nbsp;Savings page.
           </>
         )}
         {variant === 'success' && (
           <>
             Borrowing {usds} created a {dai} borrow position in Spark Lend. The borrowed {dai} was upgraded to {usds}.
-            Check your {dai} position on the portfolio and your new {usds} balance on the Cash&nbsp;&&nbsp;Savings page.
+            Check your {dai} position in the portfolio and your new {usds} balance on the Cash&nbsp;&&nbsp;Savings page.
           </>
         )}
       </p>
