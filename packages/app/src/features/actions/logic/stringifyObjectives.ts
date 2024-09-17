31bda39ee65ccd1571829f7cfd5441009dd68c9b
oskarvu <oskar.vu.dev@gmail.com>
2024-09-17 20:31:41 +0200

    Stringify objective exhaustively

--- packages/app/src/features/actions/logic/stringifyObjectives.ts
+++ packages/app/src/features/actions/logic/stringifyObjectives.ts
@@ -1,5 +1,5 @@
+import { assertNever } from '@/utils/assertNever'
 import { JSONStringifyRich } from '@/utils/object'
-
 import { Objective } from './types'
 
 export function stringifyObjectivesDeep(objectives: Objective[]): string {
@@ -8,15 +8,34 @@ export function stringifyObjectivesDeep(objectives: Objective[]): string {
 
 export function stringifyObjectivesToStableActions(objectives: Objective[]): string {
   return JSON.stringify(
-    objectives.map((o: any) => [
-      // required because of conditional creation of actions
-      o.type,
-      o?.token?.address,
-      o?.reserve?.token.address,
-      o?.useAToken,
-      // values are stringified to reload actions when inputs change (and for example new approval value is needed)
-      o?.value,
-      o?.migrateDAIToSUSDS,
-    ]),
+    objectives.map((objective) => {
+      switch (objective.type) {
+        case 'deposit':
+        case 'borrow':
+          return [objective.type, objective.token.address, objective.value]
+        case 'withdraw':
+          return [objective.type, objective.reserve.token.address, objective.value]
+        case 'repay':
+          return [objective.type, objective.reserve.token.address, objective.value, objective.useAToken]
+        case 'setUseAsCollateral':
+          return [objective.type, objective.useAsCollateral]
+        case 'setUserEMode':
+          return [objective.type, objective.eModeCategoryId]
+        case 'claimRewards':
+          return [objective.type, objective.token.address]
+        case 'withdrawFromSavings':
+          return [objective.type, objective.token.address, objective.amount]
+        case 'depositToSavings':
+          return [objective.type, objective.token.address, objective.savingsToken.address, objective.value]
+        case 'upgrade':
+          return [objective.type, objective.amount]
+        case 'downgrade':
+          return [objective.type, objective.amount]
+        case 'stake':
+          return [objective.type, objective.token, objective.amount]
+        default:
+          assertNever(objective)
+      }
+    }),
   )
 }
