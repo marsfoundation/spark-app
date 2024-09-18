3dd10dda718b1a8f5ee6776ab38c5de12d5e46db
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-18 10:24:12 +0200

    Update oracle panel typing

--- packages/app/src/features/market-details/components/oracle-panel/OraclePanel.tsx
+++ packages/app/src/features/market-details/components/oracle-panel/OraclePanel.tsx
@@ -7,21 +7,21 @@ import { UnknownOraclePanel } from './components/UnknownOraclePanel'
 import { YieldingFixedOraclePanel } from './components/YieldingFixedOraclePanel'
 
 export interface OraclePanelProps {
-  oracle: OracleType
+  oracle: OracleType | undefined
   marketInfo: MarketInfo
   chainId: number
 }
 
-export function OraclePanel(props: OraclePanelProps) {
-  switch (props?.oracle?.type) {
+export function OraclePanel({ oracle, ...props }: OraclePanelProps) {
+  switch (oracle?.type) {
     case 'fixed':
-      return <FixedOraclePanel {...props} oracle={props.oracle} />
+      return <FixedOraclePanel {...props} oracle={oracle} />
     case 'market-price':
-      return <MarketOraclePanel {...props} oracle={props.oracle} />
+      return <MarketOraclePanel {...props} oracle={oracle} />
     case 'yielding-fixed':
-      return <YieldingFixedOraclePanel {...props} oracle={props.oracle} />
+      return <YieldingFixedOraclePanel {...props} oracle={oracle} />
     case 'underlying-asset':
-      return <UnderlyingAssetOraclePanel {...props} oracle={props.oracle} />
+      return <UnderlyingAssetOraclePanel {...props} oracle={oracle} />
     default:
       return <UnknownOraclePanel {...props} />
   }
