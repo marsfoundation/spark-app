a101a78090959547fcca441726a788915d5110dc
Emil Małańczak <kdddmalanczak@gmail.com>
2024-09-23 17:21:55 +0200

    Remove ellipsis for compact address

--- packages/app/src/ui/atoms/address/Address.tsx
+++ packages/app/src/ui/atoms/address/Address.tsx
@@ -1,6 +1,7 @@
 import { CheckedAddress } from '@/domain/types/CheckedAddress'
 import { shortenAddress } from '@/ui/utils/shortenAddress'
 import { cn } from '@/ui/utils/style'
+import { cva } from 'class-variance-authority'
 
 export interface AddressProps {
   address: CheckedAddress
@@ -9,10 +10,19 @@ export interface AddressProps {
   compact?: boolean
 }
 
+const addressVariants = cva('flex-shrink whitespace-nowrap', {
+  variants: {
+    compact: {
+      true: '',
+      false: 'overflow-hidden text-ellipsis',
+    },
+  },
+})
+
 export function Address({ className, address, postfix, compact }: AddressProps) {
   return (
     <span className={cn('flex min-w-0 flex-row flex-nowrap items-center gap-2', className)} aria-label={address}>
-      <span aria-hidden="true" className="flex-shrink overflow-hidden text-ellipsis whitespace-nowrap">
+      <span aria-hidden="true" className={addressVariants({ compact })}>
         {compact ? shortenAddress(address) : address}
       </span>
       {postfix && <span className="flex-shrink-0">{postfix}</span>}
