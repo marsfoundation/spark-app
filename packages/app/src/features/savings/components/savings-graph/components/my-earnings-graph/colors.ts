610aa81ae488a4fd13c36cc8a6be2bc5a8eb311b
GitButler <gitbutler@gitbutler.com>
2024-09-30 14:17:54 +0200

    GitButler WIP Commit

---
+++ packages/app/src/features/savings/components/savings-graph/components/my-earnings-graph/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  tooltipLine: '#6A7692',
+  dot: '#0B2140',
+  dotStroke: 'white',
+  backgroundLine: '#D9D9D9',
+  axisTickLabel: '#6A7692',
+  primary: '#627EEA',
+  secondary: '#F2A52B',
+}
