2808b58346bcf54d63095eb47efbb56ffd685303
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2024-09-30 12:00:24 +0200

    Display SSR instead of DSR in empty savings state (#316)

--- packages/app/src/features/savings/views/SavingsDaiAndUSDSView.tsx
+++ packages/app/src/features/savings/views/SavingsDaiAndUSDSView.tsx
@@ -79,7 +79,7 @@ export function SavingsDaiAndUSDSView({
           />
         )}
         {displaySavingsNoCash && (
-          <SavingsOpportunityNoCash APY={sDaiDetails.APY} originChainId={originChainId} savingsMeta={savingsMeta} />
+          <SavingsOpportunityNoCash APY={sUSDSDetails.APY} originChainId={originChainId} savingsMeta={savingsMeta} />
         )}
       </div>
       <StablecoinsInWallet assets={assetsInWallet} openDialog={openDialog} migrationInfo={migrationInfo} />
