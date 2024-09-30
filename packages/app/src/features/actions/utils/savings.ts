adc55021f71480ba3c458ac7c5d5634b27387391
Krzysztof Kaczor <krzkaczor@gmail.com>
2024-09-30 14:05:11 +0200

    Refactor savings into tagged union

--- packages/app/src/features/actions/utils/savings.ts
+++ packages/app/src/features/actions/utils/savings.ts
@@ -3,6 +3,7 @@ import { TokenSymbol } from '@/domain/types/TokenSymbol'
 import { TokensInfo } from '@/domain/wallet/useTokens/TokenInfo'
 import BigNumber from 'bignumber.js'
 import { gnosis } from 'viem/chains'
+import { raise } from '@/utils/assert.ts'
 
 export function calculateGemConversionFactor({
   gemDecimals,
@@ -11,68 +12,50 @@ export function calculateGemConversionFactor({
   return BigNumber(10).pow(assetsTokenDecimals - gemDecimals)
 }
 
-export function isVaultOperation({
-  token,
-  savingsToken,
-  tokensInfo,
-  chainId,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo; chainId: number }): boolean {
-  if (isSexyDaiOperation({ token, savingsToken, tokensInfo, chainId })) {
-    return false
+export type SavingsActionType =
+  | 'usds-to-susds'
+  | 'dai-to-susds'
+  | 'usdc-to-susds'
+  | 'dai-to-sdai'
+  | 'usdc-to-sdai'
+  | 'sexy-dai-to-sdai'
+
+export function getSavingsActionType(opts: {
+  token: Token
+  savingsToken: Token
+  tokensInfo: TokensInfo
+  chainId: number
+}): SavingsActionType {
+  if (
+    opts.token.symbol === opts.tokensInfo.DAI?.symbol &&
+    opts.savingsToken.symbol === opts.tokensInfo.sDAI?.symbol &&
+    opts.chainId === gnosis.id
+  ) {
+    return 'sexy-dai-to-sdai'
   }
 
-  return (
-    (token.symbol === tokensInfo.DAI?.symbol && savingsToken.symbol === tokensInfo.sDAI?.symbol) ||
-    (token.symbol === tokensInfo.USDS?.symbol && savingsToken.symbol === tokensInfo.sUSDS?.symbol)
-  )
-}
+  if (
+    opts.token.symbol === opts.tokensInfo.USDS?.symbol &&
+    opts.savingsToken.symbol === opts.tokensInfo.sUSDS?.symbol
+  ) {
+    return 'usds-to-susds'
+  }
 
-export function isSexyDaiOperation({
-  token,
-  savingsToken,
-  tokensInfo,
-  chainId,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo; chainId: number }): boolean {
-  return (
-    token.symbol === tokensInfo.DAI?.symbol && savingsToken.symbol === tokensInfo.sDAI?.symbol && chainId === gnosis.id
-  )
-}
+  if (opts.token.symbol === opts.tokensInfo.DAI?.symbol && opts.savingsToken.symbol === opts.tokensInfo.sUSDS?.symbol) {
+    return 'dai-to-susds'
+  }
 
-export function isUsdcDaiPsmActionsOperation({
-  token,
-  savingsToken,
-  tokensInfo,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo }): boolean {
-  return token.symbol === TokenSymbol('USDC') && savingsToken.symbol === tokensInfo.sDAI?.symbol
-}
+  if (opts.token.symbol === TokenSymbol('USDC') && opts.savingsToken.symbol === opts.tokensInfo.sUSDS?.symbol) {
+    return 'usdc-to-susds'
+  }
 
-export function isUsdcUsdsPsmActionsOperation({
-  token,
-  savingsToken,
-  tokensInfo,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo }): boolean {
-  return token.symbol === TokenSymbol('USDC') && savingsToken.symbol === tokensInfo.sUSDS?.symbol
-}
+  if (opts.token.symbol === opts.tokensInfo.DAI?.symbol && opts.savingsToken.symbol === opts.tokensInfo.sDAI?.symbol) {
+    return 'dai-to-sdai'
+  }
 
-export function isUsdcPsmActionsOperation({
-  token,
-  savingsToken,
-  tokensInfo,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo }): boolean {
-  return (
-    isUsdcDaiPsmActionsOperation({ token, savingsToken, tokensInfo }) ||
-    isUsdcUsdsPsmActionsOperation({
-      token,
-      savingsToken,
-      tokensInfo,
-    })
-  )
-}
+  if (opts.token.symbol === TokenSymbol('USDC') && opts.savingsToken.symbol === opts.tokensInfo.sDAI?.symbol) {
+    return 'usdc-to-sdai'
+  }
 
-export function isSDaiToUsdsWithdraw({
-  token,
-  savingsToken,
-  tokensInfo,
-}: { token: Token; savingsToken: Token; tokensInfo: TokensInfo }): boolean {
-  return token.symbol === tokensInfo.USDS?.symbol && savingsToken.symbol === tokensInfo.sDAI?.symbol
+  raise('Savings action type not recognized')
 }
