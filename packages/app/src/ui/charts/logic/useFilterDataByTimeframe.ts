201cfd36373f7cf97220b281ca7cd15dd6efaab9
oskarvu <oskar.vu.dev@gmail.com>
2024-10-03 15:44:38 +0200

    Add explicit return type

--- packages/app/src/ui/charts/logic/useFilterDataByTimeframe.ts
+++ packages/app/src/ui/charts/logic/useFilterDataByTimeframe.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react'
 import { Timeframe } from '../defaults'
 import { filterChartData } from './filterChartData'
 
-export function useFilterChartDataByTimeframe(timeframe: Timeframe) {
+export function useFilterChartDataByTimeframe(timeframe: Timeframe): <T extends { date: Date }>(data: T[]) => T[] {
   const { timestamp, timestampInMs } = useTimestamp()
   return useCallback(
     <T extends { date: Date }>(data: T[]) => filterChartData({ data, timeframe, timestamp, timestampInMs }),
