df6e99f70a897c9942522d2b1499bf03d9a9287f
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-15 11:50:43 +0200

    Adjust savings grid columns by children count

--- packages/app/src/features/savings/components/savings-view-grid/SavingsViewGrid.tsx
+++ packages/app/src/features/savings/components/savings-view-grid/SavingsViewGrid.tsx
@@ -1,5 +1,15 @@
-import { PropsWithChildren } from 'react'
+import { cn } from '@/ui/utils/style'
+import { Children, ReactNode } from 'react'
 
-export function SavingsViewGrid({ children }: PropsWithChildren) {
-  return <div className="flex flex-col gap-6 sm:grid sm:min-h-[384px] sm:grid-cols-2">{children}</div>
+export function SavingsViewGrid({ children }: { children: ReactNode[] }) {
+  return (
+    <div
+      className={cn(
+        'flex flex-col gap-6 sm:grid sm:min-h-[384px] ',
+        Children.toArray(children).length > 1 && 'sm:grid-cols-2',
+      )}
+    >
+      {children}
+    </div>
+  )
 }
