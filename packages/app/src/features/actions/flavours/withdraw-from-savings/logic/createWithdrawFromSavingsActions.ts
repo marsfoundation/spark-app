8614ab31a96c4364f519db8ef2c6059158f3bb10
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-16 18:06:04 +0200

    Add withdraw approve action factory function

--- packages/app/src/features/actions/flavours/withdraw-from-savings/logic/createWithdrawFromSavingsActions.ts
+++ packages/app/src/features/actions/flavours/withdraw-from-savings/logic/createWithdrawFromSavingsActions.ts
@@ -6,6 +6,7 @@ import {
   usdsPsmActionsConfig,
 } from '@/config/contracts-generated'
 import { getContractAddress } from '@/domain/hooks/useContractAddress'
+import { SavingsInfo } from '@/domain/savings-info/types'
 import { CheckedAddress } from '@/domain/types/CheckedAddress'
 import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
 import { Action, ActionContext } from '@/features/actions/logic/types'
@@ -34,20 +35,14 @@ export function createWithdrawFromSavingsActions(
     receiver: objective.mode === 'send' ? objective.receiver : undefined,
   }
 
-  const savingInfo = context.savingsDaiInfo ?? context.savingsUsdsInfo
-  assert(savingInfo, 'Savings info is required for withdraw from savings action approval')
-  const sDaiValueEstimate = savingInfo.convertToAssets({ shares: objective.amount })
-
-  function getApproveAction(spender: CheckedAddress): ApproveAction {
-    return {
-      type: 'approve',
-      token: objective.savingsToken,
-      spender,
-      value: objective.isRedeem
-        ? objective.amount
-        : NormalizedUnitNumber(sDaiValueEstimate.toFixed(objective.savingsToken.decimals, BigNumber.ROUND_UP)),
-    }
-  }
+  const getSusdsApproveAction = createApproveActionFromSavingsInfo({
+    objective,
+    savingInfo: context.savingsUsdsInfo,
+  })
+  const getSdaiApproveAction = createApproveActionFromSavingsInfo({
+    objective,
+    savingInfo: context.savingsDaiInfo,
+  })
 
   const actionPath = getSavingsWithdrawActionPath({
     token: objective.token,
@@ -62,22 +57,44 @@ export function createWithdrawFromSavingsActions(
       return [withdrawAction]
 
     case 'sdai-to-usdc':
-      return [getApproveAction(getContractAddress(psmActionsConfig.address, chainId)), withdrawAction]
+      return [getSdaiApproveAction(getContractAddress(psmActionsConfig.address, chainId)), withdrawAction]
 
     case 'susds-to-usdc':
-      return [getApproveAction(getContractAddress(usdsPsmActionsConfig.address, chainId)), withdrawAction]
+      return [getSusdsApproveAction(getContractAddress(usdsPsmActionsConfig.address, chainId)), withdrawAction]
 
     case 'sdai-to-usds':
-      return [getApproveAction(getContractAddress(migrationActionsConfig.address, chainId)), withdrawAction]
+      return [getSdaiApproveAction(getContractAddress(migrationActionsConfig.address, chainId)), withdrawAction]
 
     case 'sdai-to-sexy-dai':
-      return [getApproveAction(CheckedAddress(savingsXDaiAdapterAddress[gnosis.id])), withdrawAction]
+      return [getSdaiApproveAction(CheckedAddress(savingsXDaiAdapterAddress[gnosis.id])), withdrawAction]
 
     case 'base-susds-to-usdc':
     case 'base-susds-to-usds':
-      return [getApproveAction(getContractAddress(basePsm3Address, chainId)), withdrawAction]
+      return [getSusdsApproveAction(getContractAddress(basePsm3Address, chainId)), withdrawAction]
 
     default:
       assertNever(actionPath)
+  }
+}
+
+interface CreateApproveActionFromSavingsInfoParams {
+  objective: WithdrawFromSavingsObjective
+  savingInfo?: SavingsInfo
+}
+
+function createApproveActionFromSavingsInfo({ objective, savingInfo }: CreateApproveActionFromSavingsInfoParams) {
+  return (spender: CheckedAddress): ApproveAction => {
+    // @note This assert is here to prevent raising an error while only one of the savings info is provided
+    assert(savingInfo, 'Savings info is required for withdraw from savings action approval')
+    const savingTokenValueEstimate = savingInfo.convertToAssets({ shares: objective.amount })
+
+    return {
+      type: 'approve',
+      token: objective.savingsToken,
+      spender,
+      value: objective.isRedeem
+        ? objective.amount
+        : NormalizedUnitNumber(savingTokenValueEstimate.toFixed(objective.savingsToken.decimals, BigNumber.ROUND_UP)),
+    }
   }
 }
