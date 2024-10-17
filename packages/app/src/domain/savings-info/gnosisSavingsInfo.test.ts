0857064a238986d5506cefab19824ac362b552aa
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-17 12:01:49 +0200

    Add gnosis share prediction tests

--- packages/app/src/domain/savings-info/gnosisSavingsInfo.test.ts
+++ packages/app/src/domain/savings-info/gnosisSavingsInfo.test.ts
@@ -6,7 +6,7 @@ import { GnosisSavingsInfo } from './gnosisSavingsInfo'
 
 describe(GnosisSavingsInfo.name, () => {
   describe(GnosisSavingsInfo.prototype.predictAssetsAmount.name, () => {
-    test('predicts for 5% apy', () => {
+    test('accounts for 5% apy', () => {
       const timestamp = 1000
       const shares = NormalizedUnitNumber(100)
       const savingsInfo = new GnosisSavingsInfo({
@@ -26,7 +26,7 @@ describe(GnosisSavingsInfo.name, () => {
       const timestamp = 1000
       const shares = NormalizedUnitNumber(100)
       const savingsInfo = new GnosisSavingsInfo({
-        vaultAPY: Percentage(36.5, true), // 5% / day
+        vaultAPY: Percentage(36.5, true), // 10% / day
         totalAssets: NormalizedUnitNumber(100),
         totalSupply: NormalizedUnitNumber(100),
         currentTimestamp: timestamp,
@@ -35,7 +35,54 @@ describe(GnosisSavingsInfo.name, () => {
         shares,
         timestamp: timestamp + 24 * 60 * 60,
       })
+
       expect(fivePercentYield.minus(NormalizedUnitNumber(110)).abs().lt(1e-12)).toEqual(true)
+    })
+  })
+
+  describe(GnosisSavingsInfo.prototype.predictSharesAmount.name, () => {
+    test('accounts for 5% apy', () => {
+      const timestamp = 1000
+      const assets = NormalizedUnitNumber(100)
+      const savingsInfo = new GnosisSavingsInfo({
+        vaultAPY: Percentage(18.25, true), // 5% / day
+        totalAssets: NormalizedUnitNumber(100),
+        totalSupply: NormalizedUnitNumber(100),
+        currentTimestamp: timestamp,
+      })
+      const fivePercentYield = savingsInfo.predictSharesAmount({
+        assets,
+        timestamp: timestamp + 24 * 60 * 60,
+      })
+
+      expect(
+        fivePercentYield
+          .minus(NormalizedUnitNumber(NormalizedUnitNumber(100).dividedBy(1.05)))
+          .abs()
+          .lt(1e-12),
+      ).toEqual(true)
+    })
+
+    test('accounts for 10% apy', () => {
+      const timestamp = 1000
+      const assets = NormalizedUnitNumber(100)
+      const savingsInfo = new GnosisSavingsInfo({
+        vaultAPY: Percentage(36.5, true), // 10% / day
+        totalAssets: NormalizedUnitNumber(100),
+        totalSupply: NormalizedUnitNumber(100),
+        currentTimestamp: timestamp,
+      })
+      const fivePercentYield = savingsInfo.predictSharesAmount({
+        assets,
+        timestamp: timestamp + 24 * 60 * 60,
+      })
+
+      expect(
+        fivePercentYield
+          .minus(NormalizedUnitNumber(NormalizedUnitNumber(100).dividedBy(1.1)))
+          .abs()
+          .lt(1e-12),
+      ).toEqual(true)
     })
   })
 })
