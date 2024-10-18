5439ac6faf756e956666445d7a2249cfac510ff7
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-18 19:05:30 +0200

    Add Base Savings Withdraw E2E Tests

---
+++ packages/app/src/features/dialogs/savings/withdraw/e2e/base/SendUSDC.test-e2e.ts
@@ -0,0 +1,99 @@
+import { SavingsPageObject } from '@/pages/Savings.PageObject'
+import { TOKENS_ON_FORK } from '@/test/e2e/constants'
+import { setupFork } from '@/test/e2e/forking/setupFork'
+import { setup } from '@/test/e2e/setup'
+import { randomAddress } from '@/test/utils/addressUtils'
+import { test } from '@playwright/test'
+import { base } from 'viem/chains'
+import { SavingsDialogPageObject } from '../../../common/e2e/SavingsDialog.PageObject'
+
+test.describe('Send USDC', () => {
+  const fork = setupFork({ chainId: base.id })
+
+  let savingsPage: SavingsPageObject
+  let sendDialog: SavingsDialogPageObject
+
+  const receiver = randomAddress('bob')
+  const amount = 7000
+  const usdc = TOKENS_ON_FORK[base.id].USDC
+
+  test.beforeEach(async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          ETH: 1,
+          USDS: 10_000,
+        },
+      },
+    })
+
+    savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.clickDepositButtonAction('USDS')
+    const depositDialog = new SavingsDialogPageObject({ page, type: 'deposit' })
+
+    await depositDialog.fillAmountAction(10_000)
+    await depositDialog.actionsContainer.acceptAllActionsAction(2, fork)
+    await depositDialog.clickBackToSavingsButton()
+
+    await savingsPage.clickSendSUsdsButtonAction()
+    sendDialog = new SavingsDialogPageObject({ page, type: 'send' })
+
+    await sendDialog.selectAssetAction('USDC')
+    await sendDialog.fillAmountAction(amount)
+    await sendDialog.fillReceiverAction(receiver)
+  })
+
+  test('has correct action plan', async () => {
+    await sendDialog.actionsContainer.expectActions([
+      { type: 'approve', asset: 'sUSDS' },
+      { type: 'withdrawFromSavings', asset: 'USDC', savingsAsset: 'sUSDS', mode: 'send' },
+    ])
+  })
+
+  test('displays transaction overview', async () => {
+    await sendDialog.expectNativeRouteTransactionOverview({
+      routeItems: [
+        {
+          tokenAmount: '6,963.77 sUSDS',
+          tokenUsdValue: '$7,000.00',
+        },
+        {
+          tokenAmount: '7,000.00 USDS',
+          tokenUsdValue: '$7,000.00',
+        },
+        {
+          tokenAmount: '7,000.00 USDC',
+          tokenUsdValue: '$7,000.00',
+        },
+      ],
+      outcome: '7,000.00 USDC worth $7,000.00',
+      badgeToken: 'USDC',
+    })
+  })
+
+  test('executes send', async () => {
+    await sendDialog.expectReceiverTokenBalance({
+      forkUrl: fork.forkUrl,
+      receiver,
+      token: usdc,
+      expectedBalance: 0,
+    })
+
+    await sendDialog.actionsContainer.acceptAllActionsAction(2, fork)
+    await sendDialog.expectSuccessPage()
+
+    await sendDialog.expectReceiverTokenBalance({
+      forkUrl: fork.forkUrl,
+      receiver,
+      token: usdc,
+      expectedBalance: amount,
+    })
+
+    await sendDialog.clickBackToSavingsButton()
+    await savingsPage.expectSavingsUsdsBalance({ susdsBalance: '2,984.47 sUSDS', estimatedUsdsValue: '3,000' })
+    await savingsPage.expectStablecoinsInWalletAssetBalance('USDC', '-')
+  })
+})
---
+++ packages/app/src/features/dialogs/savings/withdraw/e2e/base/WithdrawMaxUSDC.test-e2e.ts
@@ -0,0 +1,80 @@
+import { ActionsPageObject } from '@/features/actions/ActionsContainer.PageObject'
+import { SavingsPageObject } from '@/pages/Savings.PageObject'
+import { setupFork } from '@/test/e2e/forking/setupFork'
+import { setup } from '@/test/e2e/setup'
+import { test } from '@playwright/test'
+import { base } from 'viem/chains'
+import { SavingsDialogPageObject } from '../../../common/e2e/SavingsDialog.PageObject'
+
+test.describe('Withdraw Max USDC', () => {
+  const fork = setupFork({ chainId: base.id })
+  let savingsPage: SavingsPageObject
+  let withdrawDialog: SavingsDialogPageObject
+
+  test.beforeEach(async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          ETH: 1,
+          USDS: 10_000,
+        },
+      },
+    })
+
+    savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.clickDepositButtonAction('USDS')
+    const depositDialog = new SavingsDialogPageObject({ page, type: 'deposit' })
+    await depositDialog.fillAmountAction(10_000)
+    await depositDialog.actionsContainer.acceptAllActionsAction(2, fork)
+    await depositDialog.clickBackToSavingsButton()
+
+    await savingsPage.clickWithdrawSUsdsButtonAction()
+    withdrawDialog = new SavingsDialogPageObject({ page, type: 'withdraw' })
+    await withdrawDialog.selectAssetAction('USDC')
+    await withdrawDialog.clickMaxAmountAction()
+  })
+
+  test('has correct action plan', async () => {
+    await withdrawDialog.actionsContainer.expectActions([
+      { type: 'approve', asset: 'sUSDS' },
+      { type: 'withdrawFromSavings', asset: 'USDC', savingsAsset: 'sUSDS', mode: 'withdraw' },
+    ])
+  })
+
+  test('displays transaction overview', async () => {
+    await withdrawDialog.expectNativeRouteTransactionOverview({
+      routeItems: [
+        {
+          tokenAmount: '9,948.25 sUSDS',
+          tokenUsdValue: '$10,000.00',
+        },
+        {
+          tokenAmount: '10,000.00 USDS',
+          tokenUsdValue: '$10,000.00',
+        },
+        {
+          tokenAmount: '10,000.00 USDC',
+          tokenUsdValue: '$10,000.00',
+        },
+      ],
+      outcome: '10,000.00 USDC worth $10,000.00',
+      badgeToken: 'USDC',
+    })
+
+    await withdrawDialog.expectUpgradeSwitchToBeHidden()
+  })
+
+  test('executes withdraw', async () => {
+    const actionsContainer = new ActionsPageObject(withdrawDialog.locatePanelByHeader('Actions'))
+    await actionsContainer.acceptAllActionsAction(2, fork)
+
+    await withdrawDialog.expectSuccessPage()
+    await withdrawDialog.clickBackToSavingsButton()
+
+    await savingsPage.expectOpportunityStablecoinsAmount('~$10,000.00')
+    await savingsPage.expectStablecoinsInWalletAssetBalance('USDC', '10,000')
+  })
+})
