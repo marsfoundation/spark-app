ad4de196e03548ba21c55a6035f6e2914bc2f19d
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-18 12:54:05 +0200

    Add gnosis savings page e2e test

--- packages/app/src/pages/Savings.test-e2e.ts
+++ packages/app/src/pages/Savings.test-e2e.ts
@@ -1,13 +1,13 @@
 import { test } from '@playwright/test'
-import { mainnet } from 'viem/chains'
+import { gnosis, mainnet } from 'viem/chains'
 
-import { DEFAULT_BLOCK_NUMBER } from '@/test/e2e/constants'
+import { DEFAULT_BLOCK_NUMBER, GNOSIS_DEFAULT_BLOCK_NUMBER } from '@/test/e2e/constants'
 import { setupFork } from '@/test/e2e/forking/setupFork'
 import { setup } from '@/test/e2e/setup'
 
 import { SavingsPageObject } from './Savings.PageObject'
 
-test.describe('Savings', () => {
+test.describe('Savings Mainnet', () => {
   const fork = setupFork({ blockNumber: DEFAULT_BLOCK_NUMBER, chainId: mainnet.id })
 
   test('guest state', async ({ page }) => {
@@ -72,5 +72,72 @@ test.describe('Savings', () => {
     const savingsPage = new SavingsPageObject(page)
 
     await savingsPage.expectOpportunityStablecoinsAmount('~$200.00')
+  })
+})
+
+test.describe('Savings Gnosis', () => {
+  const fork = setupFork({ blockNumber: GNOSIS_DEFAULT_BLOCK_NUMBER, chainId: gnosis.id, useTenderlyVnet: true })
+
+  test('guest state', async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'not-connected',
+      },
+    })
+
+    const savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.expectAPY('10.6%')
+    await savingsPage.expectConnectWalletCTA()
+  })
+
+  test('calculates current value', async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          sDAI: 100,
+        },
+      },
+    })
+
+    const savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.expectCurrentWorth('108.780942')
+  })
+
+  test('calculates current projections', async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          sDAI: 100,
+        },
+      },
+    })
+
+    const savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.expectCurrentProjection('$0.95', '30-day')
+    await savingsPage.expectCurrentProjection('$11.53', '1-year')
+  })
+
+  test('displays the total value of stablecoins in the wallet', async ({ page }) => {
+    await setup(page, fork, {
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          XDAI: 100,
+        },
+      },
+    })
+
+    const savingsPage = new SavingsPageObject(page)
+
+    await savingsPage.expectOpportunityStablecoinsAmount('~$100.00')
   })
 })
