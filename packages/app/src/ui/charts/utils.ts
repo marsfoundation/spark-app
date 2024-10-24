a09fb56f4b1f94c6658b04162095fdd3d8965a89
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-24 16:29:37 +0200

    Remove hours from charts tooltip

--- packages/app/src/ui/charts/utils.ts
+++ packages/app/src/ui/charts/utils.ts
@@ -7,15 +7,7 @@ export function formatTooltipDate(date: Date): string {
   const month = String(date.getMonth() + 1).padStart(2, '0') // Months are zero-based
   const year = date.getFullYear()
 
-  let hours = date.getHours()
-  const minutes = String(date.getMinutes()).padStart(2, '0')
-
-  const ampm = hours >= 12 ? 'PM' : 'AM'
-  hours = hours % 12
-  hours = hours ? hours : 12 // the hour '0' should be '12'
-  const hoursString = String(hours).padStart(2, '0')
-
-  return `${day}.${month}.${year} ${hoursString}:${minutes} ${ampm}`
+  return `${day}.${month}.${year}`
 }
 
 export function formatDateTick(date: any): string {
