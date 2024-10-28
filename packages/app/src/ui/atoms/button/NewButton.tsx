8a939728f048dd2b0477d460c556c58b4c621642
Emil Małańczak <kdddmalanczak@gmail.com>
2024-10-28 11:07:39 +0100

    organise button classes

--- packages/app/src/ui/atoms/button/NewButton.tsx
+++ packages/app/src/ui/atoms/button/NewButton.tsx
@@ -6,16 +6,28 @@ import { cn } from '@/ui/utils/style'
 import { Loader } from '../loader/Loader'
 
 const buttonVariants = cva(
-  'relative isolate inline-flex select-none items-center justify-center gap-1 overflow-hidden whitespace-nowrap rounded-reskin-sm transition-colors focus-visible:bg-reskin-base-white focus-visible:text-reskin-neutral-950 focus-visible:outline-none focus-visible:ring focus-visible:ring-reskin-primary-200 focus-visible:ring-offset-0',
+  cn(
+    'relative isolate inline-flex select-none items-center justify-center gap-1 ',
+    'overflow-hidden whitespace-nowrap rounded-reskin-sm transition-colors ',
+    'focus-visible:bg-reskin-base-white focus-visible:text-reskin-neutral-950 ',
+    'focus-visible:outline-none focus-visible:ring focus-visible:ring-reskin-primary-200 focus-visible:ring-offset-0',
+  ),
   {
     variants: {
       variant: {
-        primary:
-          'before:-z-10 bg-gradient-spark-primary text-reskin-base-white before:absolute before:inset-0 active:before:bg-reskin-neutral-800 focus-visible:before:bg-reskin-base-white hover:before:bg-reskin-neutral-950 active:text-reskin-base-white before:transition-colors',
-        secondary:
-          'bg-reskin-neutral-950 text-reskin-base-white active:bg-reskin-neutral-950 hover:bg-reskin-neutral-800 active:text-reskin-base-white',
-        tertiary:
-          'border border-reskin-border-primary border-solid bg-reskin-base-white text-reskin-neutral-950 shadow-xs active:bg-reskin-neutral-100 hover:bg-reskin-neutral-50 ',
+        primary: cn(
+          'before:-z-10 bg-gradient-spark-primary text-reskin-base-white before:absolute ',
+          'active:before:bg-reskin-neutral-800 focus-visible:before:bg-reskin-base-white ',
+          'before:inset-0 hover:before:bg-reskin-neutral-950 active:text-reskin-base-white before:transition-colors',
+        ),
+        secondary: cn(
+          'bg-reskin-neutral-950 text-reskin-base-white active:bg-reskin-neutral-950 ',
+          'hover:bg-reskin-neutral-800 active:text-reskin-base-white',
+        ),
+        tertiary: cn(
+          'border border-reskin-border-primary border-solid bg-reskin-base-white',
+          'text-reskin-neutral-950 shadow-xs active:bg-reskin-neutral-100 hover:bg-reskin-neutral-50'
+        ),
         loading: 'cursor-wait bg-reskin-neutral-50 text-reskin-base-white',
         disabled: 'cursor-not-allowed bg-reskin-neutral-50 disabled:text-reskin-neutral-500',
       },
