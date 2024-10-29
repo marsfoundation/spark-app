4ec357ffdd2699cc171cd6fbf7ab9e873cf240c2
oskarvu <oskar.vu.dev@gmail.com>
2024-10-29 10:25:17 +0100

    Add stories ui helper

---
+++ packages/app/src/ui/storybook/StoryGrid.tsx
@@ -0,0 +1,13 @@
+import { cn } from '../utils/style'
+
+export function StoryGrid({ children, className }: { children: React.ReactNode; className?: string }) {
+  return (
+    <div className={cn('grid w-fit justify-items-center gap-x-12 gap-y-4 bg-white p-6', className)}>{children}</div>
+  )
+}
+
+function GridLabel({ children }: { children: React.ReactNode }) {
+  return <div className="typography-label-6 text-reskin-neutral-500">{children}</div>
+}
+
+StoryGrid.Label = GridLabel
