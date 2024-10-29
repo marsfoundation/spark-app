b620fe69c85ff9ac0eb360cc6836ba0f9f379914
oskarvu <oskar.vu.dev@gmail.com>
2024-10-29 20:09:47 +0100

    Prevent story from cutting out in chromatic

--- packages/app/src/ui/atoms/new/close-button/CloseButton.stories.tsx
+++ packages/app/src/ui/atoms/new/close-button/CloseButton.stories.tsx
@@ -1,8 +1,11 @@
+import { cn } from '@/ui/utils/style'
+import { WithClassname } from '@sb/decorators'
 import type { Meta, StoryObj } from '@storybook/react'
 import { CloseButton } from './CloseButton'
 
 const meta: Meta<typeof CloseButton> = {
   title: 'Components/Atoms/New/CloseButton',
+  decorators: [WithClassname(cn('flex h-12 w-12 items-center justify-center bg-white'))],
   component: CloseButton,
   args: {
     onClose: () => {},
