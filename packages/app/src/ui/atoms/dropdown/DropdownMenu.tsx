a8d940344f55dba35607571f523b050531f2cbf9
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-03 15:32:11 +0100

    add DropdownMenuContent variants

--- packages/app/src/ui/atoms/dropdown/DropdownMenu.tsx
+++ packages/app/src/ui/atoms/dropdown/DropdownMenu.tsx
@@ -3,6 +3,7 @@ import { Check, ChevronRight, Circle } from 'lucide-react'
 import * as React from 'react'
 
 import { cn } from '@/ui/utils/style'
+import { VariantProps, cva } from 'class-variance-authority'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
@@ -59,45 +60,69 @@ const DropdownMenuSubContent = React.forwardRef<
 ))
 DropdownMenuSubContent.displayName = DropdownMenuPrimitive.SubContent.displayName
 
+const dropdownContentVariants = cva(
+  cn(
+    'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+    'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+    'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2',
+    'data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+    'z-50 min-w-[8rem] overflow-hidden rounded-sm border border-primary',
+    'text-primary shadow-md data-[state=closed]:animate-out',
+    'data-[state=open]:animate-in',
+  ),
+  {
+    variants: {
+      variant: {
+        primary: 'bg-primary',
+        secondary: 'bg-secondary',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  },
+)
+
 const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content> & VariantProps<typeof dropdownContentVariants>
+>(({ className, sideOffset = 4, variant, ...props }, ref) => (
   <DropdownMenuPrimitive.Portal>
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
-      className={cn(
-        'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-        'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
-        'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2',
-        'data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-        'z-50 min-w-[8rem] overflow-hidden rounded-sm border border-primary',
-        'bg-primary text-primary shadow-md data-[state=closed]:animate-out',
-        'data-[state=open]:animate-in',
-        className,
-      )}
+      className={cn(dropdownContentVariants({ variant }), className)}
       {...props}
     />
   </DropdownMenuPrimitive.Portal>
 ))
 DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
 
+const dropdownItemVariants = cva(
+  cn(
+    'typography-button-2 group relative flex cursor-default select-none items-center',
+    'gap-2 rounded-sm p-4 text-primary outline-none focus:outline-none ',
+    'transition-colors data-[disabled]:pointer-events-none ',
+    'w-full justify-start data-[disabled]:opacity-50',
+  ),
+  {
+    variants: {
+      variant: {
+        primary: 'bg-primary focus:bg-secondary',
+        secondary: 'bg-primary focus:bg-tertiary',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  },
+)
+
 const DropdownMenuItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <DropdownMenuPrimitive.Item
-    ref={ref}
-    className={cn(
-      'typography-button-2 group relative flex cursor-default select-none items-center',
-      'gap-2 rounded-sm p-4 text-primary outline-none focus:outline-none',
-      'transition-colors data-[disabled]:pointer-events-none focus:bg-secondary',
-      'w-full justify-start data-[disabled]:opacity-50',
-      className,
-    )}
-    {...props}
-  />
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & VariantProps<typeof dropdownItemVariants>
+>(({ className, variant, ...props }, ref) => (
+  <DropdownMenuPrimitive.Item ref={ref} className={cn(dropdownItemVariants({ variant }), className)} {...props} />
 ))
 DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
 
