0c060808e46b259953d98509da56fb13447ca2cd
yivlad <jacenko.vlad@gmail.com>
2024-11-04 13:25:45 +0100

    Fix assertions

--- packages/app/src/features/farm-details/dialogs/stake/e2e/mainnet/StakeUSDS.test-e2e.ts
+++ packages/app/src/features/farm-details/dialogs/stake/e2e/mainnet/StakeUSDS.test-e2e.ts
@@ -61,7 +61,7 @@ test.describe('Stake USDS to SKY farm', () => {
           lowerText: 'Deposited',
         },
       },
-      outcome: '10,000.00 USDS deposited into SKY Farm',
+      outcome: '10,000.00 USD',
     })
   })
 
