f7587e63a4ad4e4a83ec5682323b46c1e1e402e9
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-05 18:07:01 +0100

    update AppLayout

--- packages/app/src/features/savings/components/PageLayout.tsx
+++ packages/app/src/features/savings/components/PageLayout.tsx
@@ -6,7 +6,7 @@ interface PageLayoutProps {
 
 export function PageLayout({ children }: PageLayoutProps) {
   return (
-    <div className="flex w-full max-w-7xl flex-1 flex-col gap-6 px-3 pt-6 pb-16 lg:mx-auto md:mx-auto md:gap-8 md:pt-10">
+    <div className="flex w-full max-w-7xl flex-1 flex-col gap-6 px-3 pt-4 pb-16 lg:mx-auto md:mx-auto md:gap-8 md:pt-4">
       {children}
     </div>
   )
