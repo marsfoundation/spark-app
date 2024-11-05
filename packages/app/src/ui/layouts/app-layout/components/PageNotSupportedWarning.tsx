248b353389029a065e4bb396829526dbf23fdaed
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-05 18:07:15 +0100

    update NotSupportedWarning

--- packages/app/src/ui/layouts/app-layout/components/PageNotSupportedWarning.tsx
+++ packages/app/src/ui/layouts/app-layout/components/PageNotSupportedWarning.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/ui/atoms/button/Button'
+import { Button } from '@/ui/atoms/new/button/Button'
 import { cn } from '@/ui/utils/style'
 import { testIds } from '@/ui/utils/testIds'
 import { AlertTriangle } from 'lucide-react'
@@ -6,40 +6,40 @@ import { AlertTriangle } from 'lucide-react'
 export interface PageNotSupportedWarningProps {
   pageName: string
   openNetworkSelectDialog: () => void
-  className?: string
 }
 
-export function PageNotSupportedWarning({
-  pageName,
-  openNetworkSelectDialog,
-  className,
-}: PageNotSupportedWarningProps) {
+export function PageNotSupportedWarning({ pageName, openNetworkSelectDialog }: PageNotSupportedWarningProps) {
   return (
-    <div
-      className={cn(
-        'sticky bottom-0 flex min-h-28 w-full flex-col items-center justify-center gap-2.5 lg:flex-row lg:gap-40',
-        'bg-white p-4 shadow-[0_0_128px_rgba(0,0,0,0.1)]',
-        className,
-      )}
-    >
-      <div className="flex gap-4">
-        <AlertTriangle className="h-8 w-8 text-[#FC4F37]" />
-        <div className="flex flex-col gap-0.5">
-          <div className="font-semibold text-primary text-sm lg:text-base">
-            {pageName} {pageName.endsWith('s') ? 'are' : 'is'} not supported on the network you are connected to.
-          </div>
-          <div className="text-prompt-foreground text-xs lg:text-sm">
-            Switch to other supported networks to unlock this page.
+    <div className="fixed inset-0 z-30">
+      <div className="fixed inset-0 bg-reskin-alpha-overlay backdrop-blur-[2px]" aria-hidden="true" />
+
+      <div
+        className={cn(
+          'absolute bottom-0 z-20 flex min-h-28 w-full flex-col items-center justify-center gap-4 lg:flex-row lg:gap-40',
+          'bg-primary p-4 shadow-[0_0_128px_rgba(0,0,0,0.1)]',
+        )}
+      >
+        <div className="flex gap-4">
+          <AlertTriangle className="h-8 w-8 text-system-error-primary" />
+          <div className="flex flex-col gap-0.5">
+            <div className="typography-label-5 lg:typography-label-4 text-primary">
+              {pageName} {pageName.endsWith('s') ? 'are' : 'is'} not supported on the network you are connected to.
+            </div>
+            <div className="typography-body-5 text-secondary">
+              Switch to other supported networks to unlock this page.
+            </div>
           </div>
         </div>
+        <Button
+          size="l"
+          variant="secondary"
+          className="w-full px-20 lg:w-fit"
+          onClick={openNetworkSelectDialog}
+          data-testid={testIds.component.SwitchNotSupportedNetworkButton}
+        >
+          Switch network
+        </Button>
       </div>
-      <Button
-        className="w-full px-20 lg:w-fit"
-        onClick={openNetworkSelectDialog}
-        data-testid={testIds.component.SwitchNotSupportedNetworkButton}
-      >
-        Switch network
-      </Button>
     </div>
   )
 }
