d8d72fbca8828c007d1a3989ce74f32690dd8ad0
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-07 09:18:05 +0100

    add compact layout todo note

--- packages/app/src/ui/layouts/PageLayout.tsx
+++ packages/app/src/ui/layouts/PageLayout.tsx
@@ -3,6 +3,9 @@ import { cn } from '../utils/style'
 
 const layoutVariants = cva('flex w-full flex-col gap-6', {
   variants: {
+    // @todo: we need to remove this compact variant in the end
+    // since we need to have consistent layout across all pages.
+    // this was to simply adjustment before new designs were complete
     compact: {
       true: 'mx-auto max-w-5xl',
     },
