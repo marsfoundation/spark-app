f9c6c28fe215775245a7f2949284937071705dd7
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-08 16:19:34 +0100

    lint fix

--- packages/app/src/features/savings/components/PageHeader.tsx
+++ packages/app/src/features/savings/components/PageHeader.tsx
@@ -2,7 +2,7 @@ import { Info } from '@/ui/molecules/info/Info'
 
 export function PageHeader() {
   return (
-    <h1 className='typography-heading-1 flex items-center gap-2'>
+    <h1 className="typography-heading-1 flex items-center gap-2">
       Savings
       <Info className="mt-1">
         Spark Savings offers a seamless way to exchange stablecoins with sUSDS and sDAI using SKY's decentralized and
