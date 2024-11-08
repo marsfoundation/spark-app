8c072f85a11ac56d1d72c6829a4bd3fa21245133
oskarvu <oskar.vu.dev@gmail.com>
2024-11-08 15:43:26 +0100

    Adjust banner style

--- packages/app/src/ui/atoms/redesign-top-banner/RedesignTopBanner.tsx
+++ packages/app/src/ui/atoms/redesign-top-banner/RedesignTopBanner.tsx
@@ -14,26 +14,26 @@ export function RedesignTopBanner({ onClose, className }: RedesignTopBannerProps
   return (
     <div
       className={cn(
-        'relative z-10 flex w-full flex-col items-center justify-center gap-2 bg-gradient-to-r from-[#414141] via-[#0e0e0e] to-[#414141] p-2.5 text-center sm:flex-row md:p-1.5',
+        'relative z-10 flex w-full flex-col items-center justify-center gap-2 bg-white p-2.5 text-center shadow-inner sm:flex-row md:p-1.5',
         className,
       )}
     >
-      <span className="typography-label-5 md:typography-label-4 text-primary-inverse">
+      <div className="typography-label-5 md:typography-label-4 text-primary">
         Welcome to the{'  '}
         <Sparkles sizeRange={[12, 20]} className="mx-1 whitespace-nowrap">
-          New Spark!
+          <span className="bg-gradient-spark-secondary bg-clip-text text-transparent">New Spark!</span>
         </Sparkles>
         {'  '}
         <br className="md:hidden" />
         Enjoy a fresh look and enhanced experience.
-      </span>
+      </div>
 
       <IconButton
         icon={XIcon}
         variant="transparent"
         size="s"
         onClick={onClose}
-        className="-translate-y-1/2 absolute top-1/2 right-3 text-white active:text-white/50 hover:text-white/70"
+        className="-translate-y-1/2 absolute top-1/2 right-3 text-black active:text-black/50 hover:text-black/70"
       />
     </div>
   )
