38e0b6279b0e677d288935acbd4fcc77e8bf6476
Emil Małańczak <kdddmalanczak@gmail.com>
2024-11-08 16:49:34 +0100

    update skeletons

--- packages/app/src/features/markets/components/skeleton/MarketsSkeleton.tsx
+++ packages/app/src/features/markets/components/skeleton/MarketsSkeleton.tsx
@@ -5,16 +5,16 @@ export function MarketsSkeleton() {
   return (
     <PageLayout className="gap-8">
       <div className="flex flex-row items-center gap-4">
-        <Skeleton className="h-12 w-56" />
+        <Skeleton className="h-[30px] w-56" />
       </div>
       <div className="grid grid-cols-2 gap-y-4 sm:grid-cols-4">
         {Array.from({ length: 4 }).map((_, index) => (
-          <Skeleton className="h-12 max-w-[144px]" key={index} />
+          <Skeleton className="h-[66px] max-w-[144px]" key={index} />
         ))}
       </div>
       <div className="flex flex-col gap-4">
         {Array.from({ length: 4 }).map((_, index) => (
-          <Skeleton className="h-12 w-full" key={index} />
+          <Skeleton className="h-[86px] w-full" key={index} />
         ))}
       </div>
     </PageLayout>
--- packages/app/src/features/savings/components/skeleton/SavingsSkeleton.tsx
+++ packages/app/src/features/savings/components/skeleton/SavingsSkeleton.tsx
@@ -5,11 +5,11 @@ export function SavingsSkeleton() {
   return (
     <PageLayout>
       <Skeleton className="h-10 w-56" />
-      <div className="grid h-[384px] grid-cols-1 gap-6 sm:grid-cols-2">
+      <div className="grid h-[398px] grid-cols-1 gap-6 sm:grid-cols-2">
         <Skeleton className="w-full" />
         <Skeleton className="w-full" />
       </div>
-      <Skeleton className="h-64 w-full" />
+      <Skeleton className="h-[280px] w-full" />
     </PageLayout>
   )
 }
