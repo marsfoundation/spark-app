093a8b0f34e4ba05f0b4be1aa11b42efc3da83d6
yivlad <jacenko.vlad@gmail.com>
2024-11-17 16:16:03 +0700

    Replace conic gradient with linear gradient in hf indicator

--- packages/app/src/ui/atoms/new/health-factor-gauge/HealthFactorGauge.tsx
+++ packages/app/src/ui/atoms/new/health-factor-gauge/HealthFactorGauge.tsx
@@ -1,5 +1,5 @@
 import { formatHealthFactor } from '@/domain/common/format'
-import { RiskLevel, healthFactorToRiskLevel, riskLevelToTitle } from '@/domain/common/risk'
+import { healthFactorToRiskLevel, riskLevelToTitle } from '@/domain/common/risk'
 import { cn } from '@/ui/utils/style'
 import { testIds } from '@/ui/utils/testIds'
 import BigNumber from 'bignumber.js'
@@ -19,7 +19,6 @@ export function HealthFactorGauge({ value, className }: HealthFactorGaugeProps) 
   const croppedValue = Math.max(GAUGE_MIN, Math.min(GAUGE_MAX, value?.toNumber() ?? 0))
   const riskLevel = healthFactorToRiskLevel(value)
   const angle = 180 - ((croppedValue - GAUGE_MIN) / (GAUGE_MAX - GAUGE_MIN)) * 180
-  const gradient = riskLevelToGradient(riskLevel)
   const maskSecondPoint = {
     x: 200 + Math.cos((angle * Math.PI) / 180) * 180,
     y: 200 - Math.sin((angle * Math.PI) / 180) * 180,
@@ -32,148 +31,178 @@ export function HealthFactorGauge({ value, className }: HealthFactorGaugeProps) 
   }
   const showIndicatorLine = riskLevel === 'healthy' || riskLevel === 'moderate' || riskLevel === 'risky'
 
+  const gradientRiskyId = `gradient-risky-${id}`
+  const gradientModerateId = `gradient-moderate-${id}`
+  const gradientHealthyId = `gradient-healthy-${id}`
+  const gradientUnknownId = `gradient-unknown-${id}`
+
+  const gradientId = (() => {
+    switch (riskLevel) {
+      case 'liquidation':
+      case 'risky':
+        return gradientRiskyId
+      case 'moderate':
+        return gradientModerateId
+      case 'healthy':
+      case 'no debt':
+        return gradientHealthyId
+      case 'unknown':
+        return gradientUnknownId
+    }
+  })()
+
   return (
-    <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 400 200" className={className}>
-      <path d="M20 200 A 180 180 0 0 1 380 200" strokeWidth="40" stroke="#373642" fill="none" />
-      <mask id={`arc-mask-${id}`}>
+    <div
+      className={cn('relative h-fit w-fit', className)}
+      style={{
+        container: 'sidebar / inline-size',
+      }}
+    >
+      <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 400 200">
+        <path d="M20 200 A 180 180 0 0 1 380 200" strokeWidth="40" stroke="#373642" fill="none" />
+        <defs>
+          <linearGradient id={gradientRiskyId}>
+            <stop offset="0%" stopColor="white" />
+            <stop offset="33.33%" stopColor="#FA5768" />
+          </linearGradient>
+          <linearGradient id={gradientModerateId}>
+            <stop offset="0%" stopColor="white" />
+            <stop offset="66.67%" stopColor="#EDAA00" />
+          </linearGradient>
+          <linearGradient id={gradientHealthyId}>
+            <stop offset="0%" stopColor="white" />
+            <stop offset="100%" stopColor="#00C2A1" />
+          </linearGradient>
+          <linearGradient id={gradientUnknownId}>
+            <stop offset="0%" stopColor="#373642" />
+            <stop offset="100%" stopColor="#373642" />
+          </linearGradient>
+        </defs>
         <path
           d={`M20 200 A 180 180 0 0 1 ${maskSecondPoint.x} ${maskSecondPoint.y}`}
           strokeWidth="40"
-          stroke="white"
+          stroke={`url(#${gradientId})`}
           fill="none"
         />
-      </mask>
-      <foreignObject width="100%" height="100%">
-        <div
-          style={{
-            width: '400px',
-            height: '400px',
-            background: gradient,
-            mask: `url(#arc-mask-${id})`,
-          }}
-        />
-      </foreignObject>
 
-      <path
-        d="M50 200 A150 150 0 0 1 122.74428876349187 71.42490489468315"
-        stroke="#FA5768"
-        strokeWidth="2"
-        fill="none"
-      />
-      <path
-        d="M127.27855696304945 68.80704392909061 A150 150 0 0 1 272.7214430369506 68.80704392909061"
-        stroke="#EDAA00"
-        strokeWidth="2"
-        fill="none"
-      />
-      <path
-        d="M277.25571123650815 71.42490489468315 A150 150 0 0 1 350 200"
-        stroke="#00C2A1"
-        strokeWidth="2"
-        fill="none"
-      />
-
-      <text x="65" y="200" fill="#908E9E" textAnchor="middle" className="typography-label-6">
-        1
-      </text>
-      <text
-        x="87.4167"
-        y="135"
-        fill="#908E9E"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        className="typography-label-6"
-      >
-        1.5
-      </text>
-      <text
-        x="135"
-        y="87.4167"
-        fill="#908E9E"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        className="typography-label-6"
-      >
-        2
-      </text>
-      <text x="200" y="70" fill="#908E9E" textAnchor="middle" dominantBaseline="middle" className="typography-label-6">
-        2.5
-      </text>
-      <text
-        x="265"
-        y="87.4167"
-        fill="#908E9E"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        className="typography-label-6"
-      >
-        3
-      </text>
-      <text
-        x="312.5833"
-        y="135"
-        fill="#908E9E"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        className="typography-label-6"
-      >
-        3.5
-      </text>
-      <text x="335" y="200" fill="#908E9E" textAnchor="middle" className="typography-label-6">
-        4
-      </text>
-
-      <path d="M26.7949 100 L61.4359 120" stroke="black" strokeWidth="1" />
-      <path d="M100 26.7949 L120, 61.4359" stroke="black" strokeWidth="1" />
-      <path d="M200 0 L200 40" stroke="black" strokeWidth="1" />
-      <path d="M300 26.7949 L280 61.4359" stroke="black" strokeWidth="1" />
-      <path d="M373.20508 100 L338.564 120" stroke="black" strokeWidth="1" />
-
-      {showIndicatorLine && (
         <path
-          d={`M${indicatorLineCoords.x1} ${indicatorLineCoords.y1} L${indicatorLineCoords.x2} ${indicatorLineCoords.y2}`}
-          stroke="white"
+          d="M50 200 A150 150 0 0 1 122.74428876349187 71.42490489468315"
+          stroke="#FA5768"
           strokeWidth="2"
+          fill="none"
         />
-      )}
+        <path
+          d="M127.27855696304945 68.80704392909061 A150 150 0 0 1 272.7214430369506 68.80704392909061"
+          stroke="#EDAA00"
+          strokeWidth="2"
+          fill="none"
+        />
+        <path
+          d="M277.25571123650815 71.42490489468315 A150 150 0 0 1 350 200"
+          stroke="#00C2A1"
+          strokeWidth="2"
+          fill="none"
+        />
 
-      <foreignObject width="100%" height="100%">
-        <div className="flex h-full w-full flex-col items-center justify-end gap-2">
-          {riskLevel !== 'unknown' && (
-            <div
-              className={cn(
-                'rounded-full p-2 font-roobert text-[16px] text-primary leading-[18px]',
-                (riskLevel === 'risky' || riskLevel === 'liquidation') && 'bg-[#EE374F]',
-                riskLevel === 'moderate' && 'bg-[#EDA902]',
-                (riskLevel === 'healthy' || riskLevel === 'no debt') && 'bg-[#00C2A1]',
-              )}
-            >
-              {riskLevelToTitle[riskLevel]}
-            </div>
-          )}
+        <text x="65" y="200" fill="#908E9E" textAnchor="middle" className="typography-label-6">
+          1
+        </text>
+        <text
+          x="87.4167"
+          y="135"
+          fill="#908E9E"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className="typography-label-6"
+        >
+          1.5
+        </text>
+        <text
+          x="135"
+          y="87.4167"
+          fill="#908E9E"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className="typography-label-6"
+        >
+          2
+        </text>
+        <text
+          x="200"
+          y="70"
+          fill="#908E9E"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className="typography-label-6"
+        >
+          2.5
+        </text>
+        <text
+          x="265"
+          y="87.4167"
+          fill="#908E9E"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className="typography-label-6"
+        >
+          3
+        </text>
+        <text
+          x="312.5833"
+          y="135"
+          fill="#908E9E"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          className="typography-label-6"
+        >
+          3.5
+        </text>
+        <text x="335" y="200" fill="#908E9E" textAnchor="middle" className="typography-label-6">
+          4
+        </text>
+
+        <path d="M26.7949 100 L61.4359 120" stroke="black" strokeWidth="1" />
+        <path d="M100 26.7949 L120, 61.4359" stroke="black" strokeWidth="1" />
+        <path d="M200 0 L200 40" stroke="black" strokeWidth="1" />
+        <path d="M300 26.7949 L280 61.4359" stroke="black" strokeWidth="1" />
+        <path d="M373.20508 100 L338.564 120" stroke="black" strokeWidth="1" />
+
+        {showIndicatorLine && (
+          <path
+            d={`M${indicatorLineCoords.x1} ${indicatorLineCoords.y1} L${indicatorLineCoords.x2} ${indicatorLineCoords.y2}`}
+            stroke="white"
+            strokeWidth="2"
+          />
+        )}
+      </svg>
+      <div className="absolute inset-0 flex h-full w-full flex-col items-center justify-end gap-2">
+        {riskLevel !== 'unknown' && (
           <div
-            className="text-[56px] text-white leading-[56px]"
-            data-testid={testIds.component.HealthFactorGauge.value}
+            className={cn(
+              'rounded-full p-2 font-roobert text-primary',
+              (riskLevel === 'risky' || riskLevel === 'liquidation') && 'bg-[#EE374F]',
+              riskLevel === 'moderate' && 'bg-[#EDA902]',
+              (riskLevel === 'healthy' || riskLevel === 'no debt') && 'bg-[#00C2A1]',
+            )}
+            style={{
+              fontSize: '4cqw',
+              lineHeight: '4.5cqw',
+            }}
           >
-            {riskLevel === 'unknown' ? '-' : formatHealthFactor(value)}
+            {riskLevelToTitle[riskLevel]}
           </div>
+        )}
+        <div
+          className="text-white"
+          data-testid={testIds.component.HealthFactorGauge.value}
+          style={{
+            fontSize: '14cqw',
+            lineHeight: '14cqw',
+          }}
+        >
+          {riskLevel === 'unknown' ? '-' : formatHealthFactor(value)}
         </div>
-      </foreignObject>
-    </svg>
+      </div>
+    </div>
   )
-}
-
-function riskLevelToGradient(riskLevel: RiskLevel): string {
-  switch (riskLevel) {
-    case 'liquidation':
-    case 'risky':
-      return 'conic-gradient(from 270deg, white, #FA5768 16.67%)'
-    case 'moderate':
-      return 'conic-gradient(from 270deg, white, #EDAA00 33.33%)'
-    case 'healthy':
-    case 'no debt':
-      return 'conic-gradient(from 270deg, white, #00C2A1 50%)'
-    case 'unknown':
-      return 'conic-gradient(from 270deg, #373642, #373642 100%)'
-  }
 }
