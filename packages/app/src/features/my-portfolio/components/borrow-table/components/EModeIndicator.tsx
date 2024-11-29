23ca36b63f337d0610a665f7b70b6e1c72bfffb7
oskarvu <oskar.vu.dev@gmail.com>
2024-11-29 20:18:13 +0700

    Add test id to emode button

--- packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
+++ packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
@@ -2,6 +2,7 @@ import { EModeCategoryId } from '@/domain/e-mode/types'
 import { Button } from '@/ui/atoms/new/button/Button'
 
 import { EModeBadge } from '@/ui/molecules/e-mode-badge/EModeBadge'
+import { testIds } from '@/ui/utils/testIds'
 import { useBreakpoint } from '@/ui/utils/useBreakpoint'
 
 interface EModeIndicatorProps {
@@ -14,7 +15,7 @@ export function EModeIndicator({ eModeCategoryId, onButtonClick }: EModeIndicato
   return (
     <div className="flex w-full items-center justify-between gap-2">
       <EModeBadge categoryId={eModeCategoryId} />
-      <Button size="s" variant="tertiary" onClick={onButtonClick}>
+      <Button size="s" variant="tertiary" onClick={onButtonClick} data-testid={testIds.component.EModeButton}>
         {isNotMobile && 'Change'} E-Mode
       </Button>
     </div>
