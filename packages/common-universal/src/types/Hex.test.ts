ad74c65cc09dba42c5b6908fbf77b962e3c3d205
Krzysztof Kaczor <krzkaczor@gmail.com>
2024-12-01 11:49:33 +0100

    Add Hex allow empty flag

--- packages/common-universal/src/types/Hex.test.ts
+++ packages/common-universal/src/types/Hex.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'earl'
 import { describe, it } from 'mocha'
 
 import { Hex } from './Hex'
+import { hexToNumber, hexToString } from 'viem'
 
 describe(Hex.name, () => {
   it('works with an argument correctly representing base value', () => {
@@ -16,11 +17,17 @@ describe(Hex.name, () => {
     expect(() => Hex('1234567890abcdefABCDEF')).toThrow('Invalid hex: 1234567890abcdefABCDEF')
   })
 
-  it('throws if value argument does not have any characters after "0x"', () => {
-    expect(() => Hex('0x')).toThrow('Invalid hex: 0x')
-  })
-
   it('throws if value argument has characters from outside of the range', () => {
     expect(() => Hex('0xNotARealHex')).toThrow('Invalid hex: 0xNotARealHex')
+  })
+
+  describe('allowEmpty flag', () => {
+    it('throws on empty hex when not allowed', () => {
+      expect(() => Hex('0x')).toThrow('Invalid hex: 0x')
+    })
+
+    it('works with empty hex when allowed', () => {
+      expect(Hex('0x', { allowEmpty: true })).toEqual('0x' as Hex)
+    })
   })
 })
--- packages/common-universal/src/types/Hex.ts
+++ packages/common-universal/src/types/Hex.ts
@@ -5,10 +5,15 @@ import { Opaque } from './Opaque'
 
 /**
  * Any hex value i.e. 0x1234567890abcdefABCDEF.
- * Has to start with "0x"
+ * Has to start with "0x".
+ * If allowEmpty is true, then empty hex "0x" is allowed. Sometimes data is modeled this way e.g. empty log data is represented as "0x".
  */
 export type Hex = Opaque<ViemHex, 'Hex'>
-export function Hex(hex: string): Hex {
+export function Hex(hex: string, { allowEmpty = false } = {}): Hex {
+  if (allowEmpty && hex === '0x') {
+    return hex as Hex
+  }
+
   assert(hex.match(/^0x[0-9a-fA-F]+$/), `Invalid hex: ${hex}`)
   return hex as Hex
 }
