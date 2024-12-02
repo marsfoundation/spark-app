03f9825cb34e9a5d70ffc20f19e1a671bdc1fc47
oskarvu <oskar.vu.dev@gmail.com>
2024-12-02 20:12:46 +0700

    Do not use media query hook to hide part of a text

--- packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
+++ packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
@@ -2,7 +2,6 @@ import { EModeCategoryId } from '@/domain/e-mode/types'
 import { Button } from '@/ui/atoms/new/button/Button'
 import { EModeBadge } from '@/ui/molecules/e-mode-badge/EModeBadge'
 import { testIds } from '@/ui/utils/testIds'
-import { useBreakpoint } from '@/ui/utils/useBreakpoint'
 
 interface EModeIndicatorProps {
   eModeCategoryId: EModeCategoryId
@@ -10,12 +9,17 @@ interface EModeIndicatorProps {
 }
 
 export function EModeIndicator({ eModeCategoryId, onButtonClick }: EModeIndicatorProps) {
-  const isNotMobile = useBreakpoint('sm')
   return (
-    <div className="flex w-full items-center justify-between gap-2">
+    <div className="flex w-full items-center justify-between">
       <EModeBadge categoryId={eModeCategoryId} />
-      <Button size="s" variant="tertiary" onClick={onButtonClick} data-testid={testIds.component.EModeButton}>
-        {isNotMobile && 'Change'} E-Mode
+      <Button
+        size="s"
+        spacing="s"
+        variant="tertiary"
+        onClick={onButtonClick}
+        data-testid={testIds.component.EModeButton}
+      >
+        <span className="hidden sm:inline">Change</span> E-Mode
       </Button>
     </div>
   )
