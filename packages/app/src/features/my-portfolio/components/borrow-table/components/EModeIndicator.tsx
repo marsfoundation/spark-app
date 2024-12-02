10493d651e3cd52476c54bed83464b8587179a8d
Oskar <43062492+oskarvu@users.noreply.github.com>
2024-12-02 22:19:48 +0700

    Improve e-mode dialog tiles and borrow table indicator (#459)
    * Improve e-mode dialog and borrow table header
    
    * Add test id to emode button
    
    * Add id to emode badge and fix tests
    
    * Do not modify tailwind config for now
    
    * Remove redundant stories
    
    * Revert to 3 column grid for emode tiles on mobile
    
    * Do not use media query hook to hide part of a text

--- packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
+++ packages/app/src/features/my-portfolio/components/borrow-table/components/EModeIndicator.tsx
@@ -1,7 +1,7 @@
 import { EModeCategoryId } from '@/domain/e-mode/types'
-import { Info } from '@/ui/molecules/info/Info'
-
-import { EModeButton } from './EModeButton'
+import { Button } from '@/ui/atoms/new/button/Button'
+import { EModeBadge } from '@/ui/molecules/e-mode-badge/EModeBadge'
+import { testIds } from '@/ui/utils/testIds'
 
 interface EModeIndicatorProps {
   eModeCategoryId: EModeCategoryId
@@ -10,12 +10,17 @@ interface EModeIndicatorProps {
 
 export function EModeIndicator({ eModeCategoryId, onButtonClick }: EModeIndicatorProps) {
   return (
-    <div className="mt-1.5 ml-4 flex items-center gap-2" onClick={(e) => e.stopPropagation()}>
-      <div className="flex flex-row items-center gap-0.5">
-        <div className="typography-label-5 text-secondary">E-Mode</div>
-        <Info>Efficiency mode (E-Mode) increases your LTV for a selected category of assets up to 97%.</Info>
-      </div>
-      <EModeButton categoryId={eModeCategoryId} onClick={onButtonClick} />
+    <div className="flex w-full items-center justify-between">
+      <EModeBadge categoryId={eModeCategoryId} />
+      <Button
+        size="s"
+        spacing="s"
+        variant="tertiary"
+        onClick={onButtonClick}
+        data-testid={testIds.component.EModeButton}
+      >
+        <span className="hidden sm:inline">Change</span> E-Mode
+      </Button>
     </div>
   )
 }
