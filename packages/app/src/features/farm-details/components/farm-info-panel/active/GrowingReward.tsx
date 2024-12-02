8d86a74dbba5e3269305b35fe5cfa78a5f3f027b
oskarvu <oskar.vu.dev@gmail.com>
2024-12-02 20:02:15 +0700

    Restyle farm reward counter

--- packages/app/src/features/farm-details/components/farm-info-panel/active/GrowingReward.tsx
+++ packages/app/src/features/farm-details/components/farm-info-panel/active/GrowingReward.tsx
@@ -1,6 +1,7 @@
 import { NormalizedUnitNumber } from '@/domain/types/NumericValues'
 import { Token } from '@/domain/types/Token'
 import { getTokenImage } from '@/ui/assets'
+import { cn } from '@/ui/utils/style'
 import { testIds } from '@/ui/utils/testIds'
 import { getFractionalPart, getWholePart } from '@/utils/bigNumber'
 import { useTimestamp } from '@/utils/useTimestamp'
@@ -23,23 +24,33 @@ export function GrowingReward({ rewardToken, calculateReward, refreshIntervalInM
   const precision = calculatePrecision({ currentReward, rewardIn1Step })
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex items-baseline gap-2">
-        <img src={getTokenImage(rewardToken.symbol)} className="h-8 w-8" />
+    <div className="isolate grid grid-cols-[auto_1fr] items-center gap-x-2 lg:gap-x-4 lg:gap-y-2 xl:gap-y-3">
+      <img src={getTokenImage(rewardToken.symbol)} className="h-8 shrink-0 lg:h-12 xl:h-14" />
+      <div className="flex items-baseline tabular-nums" data-testid={testIds.farmDetails.activeFarmInfoPanel.rewards}>
         <div
-          className="flex flex-row items-baseline justify-center tabular-nums"
-          data-testid={testIds.farmDetails.activeFarmInfoPanel.rewards}
+          className={cn(
+            'typography-heading-3 lg:typography-display-3 xl:typography-display-2 relative bg-clip-text text-transparent',
+            'before:-z-10 before:-inset-2 before:absolute before:bg-reskin-base-black before:blur-sm',
+            'bg-gradient-farms-1',
+          )}
         >
-          <div className="typography-heading-2 sm:typography-heading-3 xl:typography-heading-1 text-primary">
-            {getWholePart(currentReward)}
-          </div>
-          <div className="typography-heading-5 sm:typography-heading-5 xl:typography-heading-4 text-primary">
-            {getFractionalPart(currentReward, precision)}
-          </div>
+          {getWholePart(currentReward)}
         </div>
+        {precision > 0 && (
+          <div className="relative">
+            <div
+              className={cn(
+                'typography-heading-4 before:-z-10 before:absolute before:inset-0 before:bg-reskin-base-black before:blur-sm',
+                'text-feature-farms-primary',
+              )}
+            >
+              {getFractionalPart(currentReward, precision)}
+            </div>
+          </div>
+        )}
       </div>
       {rewardToken.unitPriceUsd.gt(0) && (
-        <div className="typography-label-5 text-secondary">
+        <div className="typography-label-6 col-start-2 ml-1.5 text-primary-inverse">
           &#8776;
           <span data-testid={testIds.farmDetails.activeFarmInfoPanel.rewardsUsd}>
             {rewardToken.formatUSD(currentReward)}
