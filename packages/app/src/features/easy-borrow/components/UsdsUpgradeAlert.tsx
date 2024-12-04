73da5a976dd55269b7ec181b4864085117cf342b
oskarvu <oskar.vu.dev@gmail.com>
2024-12-04 18:41:14 +0700

    Improve look of easy borrow success view

--- packages/app/src/features/easy-borrow/components/UsdsUpgradeAlert.tsx
+++ packages/app/src/features/easy-borrow/components/UsdsUpgradeAlert.tsx
@@ -12,21 +12,19 @@ export function UsdsUpgradeAlert({ borrowDetails, variant, className }: UsdsUpgr
   const { dai, usds } = borrowDetails
   return (
     <Alert variant="info" className={className} data-testid={testIds.easyBorrow.form.usdsBorrowAlert}>
-      <p className="max-w-[82ch]">
-        {variant === 'borrow' && (
-          <>
-            Borrowing {usds} creates a {dai} borrow position in Spark Lend. The borrowed {dai} is upgraded to {usds} in
-            a separate action listed in the actions panel below. You will see your {dai} position in the portfolio, and
-            your new {usds} balance will be reflected on the Savings page.
-          </>
-        )}
-        {variant === 'success' && (
-          <>
-            Borrowing {usds} created a {dai} borrow position in Spark Lend. The borrowed {dai} was upgraded to {usds}.
-            Check your {dai} position in the portfolio and your new {usds} balance on the Savings page.
-          </>
-        )}
-      </p>
+      {variant === 'borrow' && (
+        <>
+          Borrowing {usds} creates a {dai} borrow position in Spark Lend. The borrowed {dai} is upgraded to {usds} in a
+          separate action listed in the actions panel below. You will see your {dai} position in the portfolio, and your
+          new {usds} balance will be reflected on the Savings page.
+        </>
+      )}
+      {variant === 'success' && (
+        <>
+          Borrowing {usds} created a {dai} borrow position in Spark Lend. The borrowed {dai} was upgraded to {usds}.
+          Check your {dai} position in the portfolio and your new {usds} balance on the Savings page.
+        </>
+      )}
     </Alert>
   )
 }
