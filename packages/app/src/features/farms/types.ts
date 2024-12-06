ab409c1050a9568e3ed687154508a2f30a7f5d49
oskarvu <oskar.vu.dev@gmail.com>
2024-12-06 18:19:27 +0700

    Delete unused files

--- packages/app/src/features/farms/types.ts
+++
@@ -1,9 +0,0 @@
-import { Token } from '@/domain/types/Token'
-import { NormalizedUnitNumber, Percentage } from '@marsfoundation/common-universal'
-
-export interface FarmInfo {
-  apy: Percentage
-  rewardToken: Token
-  stakingToken: Token
-  deposit: NormalizedUnitNumber
-}
