cb7177911c9782a4b289c5f89d6884164a3894a2
Oskar <43062492+oskarvu@users.noreply.github.com>
2024-12-19 19:38:54 +0700

    Rewrite farm stake dialog tests (#507)
    * Rewrite stake usds tests
    
    * Add stake sdai test
    
    * Add stake susds tests
    
    * Add stake dai tests
    
    * Add stake usdc tests

--- packages/app/src/features/dialogs/savings/common/e2e/SavingsDialog.PageObject.ts
+++ packages/app/src/features/dialogs/savings/common/e2e/SavingsDialog.PageObject.ts
@@ -1,151 +1,163 @@
-// import { getBalance, getTokenBalance } from '@/test/e2e/utils'
-// import { testIds } from '@/ui/utils/testIds'
-// import { Locator, Page, expect } from '@playwright/test'
-// import { Address } from 'viem'
-// import { DialogPageObject, TxOverviewWithRoute } from '../../../common/Dialog.PageObject'
+import { TestContext } from '@/test/e2e/setup'
+import { getBalance, getTokenBalance } from '@/test/e2e/utils'
+import { testIds } from '@/ui/utils/testIds'
+import { getUrlFromClient } from '@marsfoundation/common-testnets'
+import { Locator, expect } from '@playwright/test'
+import { Address } from 'viem'
+import { DialogPageObject, TxOverviewWithRoute } from '../../../common/Dialog.PageObject'
 
-// export class SavingsDialogPageObject extends DialogPageObject {
-//   private readonly type: 'deposit' | 'withdraw' | 'send'
+export interface SavingsDialogPageObjectParams {
+  testContext: TestContext<any>
+  type: 'deposit' | 'withdraw' | 'send'
+}
 
-//   constructor({ page, type }: { page: Page; type: 'deposit' | 'withdraw' | 'send' }) {
-//     super(
-//       page,
-//       new RegExp(`${type === 'deposit' ? 'Deposit to' : type === 'send' ? 'Send from' : 'Withdraw from'} Savings`),
-//     )
-//     this.type = type
-//   }
+export class SavingsDialogPageObject extends DialogPageObject {
+  private readonly type: 'deposit' | 'withdraw' | 'send'
 
-//   // # region locators
-//   locateUpgradeSwitch(): Locator {
-//     return this.page.getByTestId(testIds.dialog.savings.upgradeSwitch)
-//   }
-//   // #endregion locators
+  constructor({ testContext, type }: SavingsDialogPageObjectParams) {
+    super({
+      testContext,
+      header: new RegExp(
+        `${type === 'deposit' ? 'Deposit to' : type === 'send' ? 'Send from' : 'Withdraw from'} Savings`,
+      ),
+    })
+    this.type = type
+  }
 
-//   // #region actions
-//   async clickBackToSavingsButton(): Promise<void> {
-//     await this.page.getByRole('button', { name: 'Back to Savings' }).click()
-//     await this.region.waitFor({
-//       state: 'detached',
-//     })
-//   }
+  // # region locators
+  locateUpgradeSwitch(): Locator {
+    return this.page.getByTestId(testIds.dialog.savings.upgradeSwitch)
+  }
+  // #endregion locators
 
-//   async fillReceiverAction(receiver: string): Promise<void> {
-//     await this.region.getByTestId(testIds.component.AddressInput.input).fill(receiver)
-//   }
+  // #region actions
+  async clickBackToSavingsButton(): Promise<void> {
+    await this.page.getByRole('button', { name: 'Back to Savings' }).click()
+    await this.region.waitFor({
+      state: 'detached',
+    })
+  }
 
-//   // #endregion actions
+  async fillReceiverAction(receiver: string): Promise<void> {
+    await this.region.getByTestId(testIds.component.AddressInput.input).fill(receiver)
+  }
 
-//   // #region assertions
-//   async expectDiscrepancyWarning(discrepancy: string): Promise<void> {
-//     const explanation =
-//       this.type === 'deposit'
-//         ? 'The final amount received may be less than the deposit amount by up to'
-//         : 'You may be charged more than the withdraw amount by up to'
-//     await expect(
-//       this.region.getByText(`Market fluctuations can impact your transaction value. ${explanation} ${discrepancy}`),
-//     ).toBeVisible()
-//   }
+  // #endregion actions
 
-//   async expectTransactionOverviewToBeVisible(): Promise<void> {
-//     await expect(this.locatePanelByHeader('Transaction overview')).toBeVisible()
-//   }
+  // #region assertions
+  async expectDiscrepancyWarning(discrepancy: string): Promise<void> {
+    const explanation =
+      this.type === 'deposit'
+        ? 'The final amount received may be less than the deposit amount by up to'
+        : 'You may be charged more than the withdraw amount by up to'
+    await expect(
+      this.region.getByText(`Market fluctuations can impact your transaction value. ${explanation} ${discrepancy}`),
+    ).toBeVisible()
+  }
 
-//   async expectAssetSelectorOptions(options: string[]): Promise<void> {
-//     const selectorOptions = await this.page.getByTestId(testIds.component.AssetSelector.option).all()
-//     expect(selectorOptions).toHaveLength(options.length)
+  async expectTransactionOverviewToBeVisible(): Promise<void> {
+    await expect(this.locatePanelByHeader('Transaction overview')).toBeVisible()
+  }
 
-//     for (const [index, option] of selectorOptions.entries()) {
-//       await expect(option).toContainText(options[index]!)
-//     }
-//   }
+  async expectAssetSelectorOptions(options: string[]): Promise<void> {
+    const selectorOptions = await this.page.getByTestId(testIds.component.AssetSelector.option).all()
+    expect(selectorOptions).toHaveLength(options.length)
 
-//   async expectTransactionOverview(transactionOverview: TransactionOverview): Promise<void> {
-//     const panel = this.locatePanelByHeader('Transaction overview')
-//     await expect(panel).toBeVisible()
+    for (const [index, option] of selectorOptions.entries()) {
+      await expect(option).toContainText(options[index]!)
+    }
+  }
 
-//     for (const [index, [label, value]] of transactionOverview.entries()) {
-//       const row = panel.getByTestId(testIds.dialog.depositSavings.transactionDetailsRow(index))
-//       await expect(row).toBeVisible()
-//       await expect(row).toContainText(label)
-//       await expect(row).toContainText(value)
-//     }
-//   }
+  async expectTransactionOverview(transactionOverview: TransactionOverview): Promise<void> {
+    const panel = this.locatePanelByHeader('Transaction overview')
+    await expect(panel).toBeVisible()
 
-//   async expectNativeRouteTransactionOverview(transactionOverview: NativeRouteTransactionOverview): Promise<void> {
-//     const panel = this.locatePanelByHeader('Transaction overview')
-//     await expect(panel).toBeVisible()
-//     const savingsTxOverviewTestIds = testIds.dialog.savings.transactionOverview
+    for (const [index, [label, value]] of transactionOverview.entries()) {
+      const row = panel.getByTestId(testIds.dialog.depositSavings.transactionDetailsRow(index))
+      await expect(row).toBeVisible()
+      await expect(row).toContainText(label)
+      await expect(row).toContainText(value)
+    }
+  }
 
-//     if (transactionOverview.apy) {
-//       const apyValue = panel.getByTestId(savingsTxOverviewTestIds.apy.value)
-//       const apyDescription = panel.getByTestId(savingsTxOverviewTestIds.apy.description)
-//       await expect(apyValue).toContainText(transactionOverview.apy.value)
-//       await expect(apyDescription).toContainText(transactionOverview.apy.description)
-//     }
+  async expectNativeRouteTransactionOverview(transactionOverview: NativeRouteTransactionOverview): Promise<void> {
+    const panel = this.locatePanelByHeader('Transaction overview')
+    await expect(panel).toBeVisible()
+    const savingsTxOverviewTestIds = testIds.dialog.savings.transactionOverview
 
-//     await this.expectTransactionOverviewRoute(transactionOverview.routeItems)
-//     if (transactionOverview.badgeTokens) {
-//       await this.expectSkyBadgeForTokens(transactionOverview.badgeTokens)
-//     }
-//     await this.expectOutcomeText(transactionOverview.outcome)
-//   }
+    if (transactionOverview.apy) {
+      const apyValue = panel.getByTestId(savingsTxOverviewTestIds.apy.value)
+      const apyDescription = panel.getByTestId(savingsTxOverviewTestIds.apy.description)
+      await expect(apyValue).toContainText(transactionOverview.apy.value)
+      await expect(apyDescription).toContainText(transactionOverview.apy.description)
+    }
 
-//   async expectSuccessPage(): Promise<void> {
-//     // for now we only check if the success message is visible
-//     await expect(this.page.getByText('Congrats, all done!')).toBeVisible()
-//   }
+    await this.expectTransactionOverviewRoute(transactionOverview.routeItems)
+    if (transactionOverview.badgeTokens) {
+      await this.expectSkyBadgeForTokens(transactionOverview.badgeTokens)
+    }
+    await this.expectOutcomeText(transactionOverview.outcome)
+  }
 
-//   async expectAddressInputError(error: string): Promise<void> {
-//     await expect(this.page.getByTestId(testIds.component.AddressInput.error)).toHaveText(error)
-//   }
+  async expectSuccessPage(): Promise<void> {
+    // for now we only check if the success message is visible
+    await expect(this.page.getByText('Congrats, all done!')).toBeVisible()
+  }
 
-//   async expectReceiverIsSmartContractWarning(): Promise<void> {
-//     await expect(this.page.getByTestId(testIds.dialog.savings.send.addressIsSmartContractWarning)).toBeVisible()
-//   }
+  async expectAddressInputError(error: string): Promise<void> {
+    await expect(this.page.getByTestId(testIds.component.AddressInput.error)).toHaveText(error)
+  }
 
-//   async expectReceiverBalance({
-//     forkUrl,
-//     receiver,
-//     expectedBalance,
-//   }: {
-//     forkUrl: string
-//     receiver: Address
-//     expectedBalance: number
-//   }): Promise<void> {
-//     const currentBalance = await getBalance({ forkUrl, address: receiver })
-//     expect(currentBalance.isEqualTo(expectedBalance)).toBe(true)
-//   }
+  async expectReceiverIsSmartContractWarning(): Promise<void> {
+    await expect(this.page.getByTestId(testIds.dialog.savings.send.addressIsSmartContractWarning)).toBeVisible()
+  }
 
-//   async expectReceiverTokenBalance({
-//     forkUrl,
-//     receiver,
-//     token,
-//     expectedBalance,
-//   }: {
-//     forkUrl: string
-//     receiver: Address
-//     token: { address: Address; decimals: number }
-//     expectedBalance: number
-//   }): Promise<void> {
-//     const currentTokenBalance = await getTokenBalance({ forkUrl, address: receiver, token })
-//     expect(currentTokenBalance.isEqualTo(expectedBalance)).toBe(true)
-//   }
+  async expectReceiverBalance({
+    receiver,
+    expectedBalance,
+  }: {
+    receiver: Address
+    expectedBalance: number
+  }): Promise<void> {
+    const currentBalance = await getBalance({
+      forkUrl: getUrlFromClient(this.testContext.testnetController.client),
+      address: receiver,
+    })
+    expect(currentBalance.isEqualTo(expectedBalance)).toBe(true)
+  }
 
-//   expectUpgradeSwitchToBeHidden(): Promise<void> {
-//     return expect(this.locateUpgradeSwitch()).toBeHidden()
-//   }
+  async expectReceiverTokenBalance({
+    receiver,
+    token,
+    expectedBalance,
+  }: {
+    receiver: Address
+    token: { address: Address; decimals: number }
+    expectedBalance: number
+  }): Promise<void> {
+    const currentTokenBalance = await getTokenBalance({
+      forkUrl: getUrlFromClient(this.testContext.testnetController.client),
+      address: receiver,
+      token,
+    })
+    expect(currentTokenBalance.isEqualTo(expectedBalance)).toBe(true)
+  }
 
-//   clickUpgradeSwitch(): Promise<void> {
-//     return this.locateUpgradeSwitch().click()
-//   }
-//   // #endregion assertions
-// }
+  expectUpgradeSwitchToBeHidden(): Promise<void> {
+    return expect(this.locateUpgradeSwitch()).toBeHidden()
+  }
 
-// type TransactionOverview = [string, string][]
+  clickUpgradeSwitch(): Promise<void> {
+    return this.locateUpgradeSwitch().click()
+  }
+  // #endregion assertions
+}
 
-// interface NativeRouteTransactionOverview extends TxOverviewWithRoute {
-//   apy?: {
-//     value: string
-//     description: string
-//   }
-// }
+type TransactionOverview = [string, string][]
+
+interface NativeRouteTransactionOverview extends TxOverviewWithRoute {
+  apy?: {
+    value: string
+    description: string
+  }
+}
