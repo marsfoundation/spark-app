aa52251be6193d0920af497b51f4f701834450df
yivlad <jacenko.vlad@gmail.com>
2024-12-19 12:41:10 +0100

    Change My earnings chart timeframes

--- packages/app/src/domain/savings-charts/useMyEarningsInfo/calculatePredictions.ts
+++ packages/app/src/domain/savings-charts/useMyEarningsInfo/calculatePredictions.ts
@@ -1,77 +1,23 @@
 import { SavingsInfo } from '@/domain/savings-info/types'
 import { range } from '@/utils/array'
-import { assertNever } from '@/utils/assertNever'
 import { NormalizedUnitNumber } from '@marsfoundation/common-universal'
-import { MyEarningsTimeframe } from './common'
 import { MyEarningsInfoItem } from './types'
 
 const SECONDS_PER_DAY = 24 * 60 * 60
 
-export interface CalculatePredictionsParams {
-  timestamp: number // in seconds
-  shares: NormalizedUnitNumber
-  savingsInfo: SavingsInfo
-  timeframe: MyEarningsTimeframe
-  dataLength: number
-}
-
 export function calculatePredictions({
-  timestamp,
-  shares,
-  savingsInfo,
-  timeframe,
-  dataLength,
-}: CalculatePredictionsParams): MyEarningsInfoItem[] {
-  const optimalPredictionsLength = Math.floor(dataLength * 1.66)
-
-  switch (timeframe) {
-    case '7D':
-      return calculatePredictionsIncomeByDays({
-        days: Math.max(optimalPredictionsLength, 7),
-        shares,
-        timestamp,
-        savingsInfo,
-        step: 1,
-      })
-
-    case '1M':
-      return calculatePredictionsIncomeByDays({
-        days: Math.max(optimalPredictionsLength, 30),
-        shares,
-        timestamp,
-        savingsInfo,
-        step: 1,
-      })
-
-    case '1Y':
-    case 'All':
-      return calculatePredictionsIncomeByDays({
-        // setting upper bounds due to visible performance problems
-        days: Math.max(Math.min(optimalPredictionsLength, 360), 90),
-        shares,
-        timestamp,
-        savingsInfo,
-        step: 3,
-      })
-
-    default:
-      assertNever(timeframe)
-  }
-}
-
-function calculatePredictionsIncomeByDays({
   days,
   savingsInfo,
   shares,
   timestamp,
-  step,
 }: {
   days: number
   savingsInfo: SavingsInfo
   shares: NormalizedUnitNumber
   timestamp: number
-  step: number
 }): MyEarningsInfoItem[] {
+  const step = days > 366 ? 3 : 1
+
   // @note For today we have only current balance (with slight delay) but we need also balance for next data-point
   return range(0, days, step).map((day) => {
     const dayTimestamp = timestamp + day * SECONDS_PER_DAY
