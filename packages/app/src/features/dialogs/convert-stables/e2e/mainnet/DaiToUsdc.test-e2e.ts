3a8aa846a0307936c0b17dba5c7c305c372577f3
Oskar <43062492+oskarvu@users.noreply.github.com>
2024-12-21 21:31:22 +0700

    Rewrite convert stables tests (#517)

--- packages/app/src/features/dialogs/convert-stables/e2e/mainnet/DaiToUsdc.test-e2e.ts
+++ packages/app/src/features/dialogs/convert-stables/e2e/mainnet/DaiToUsdc.test-e2e.ts
@@ -1,67 +1,69 @@
-// import { SavingsPageObject } from '@/pages/Savings.PageObject'
-// import { DEFAULT_BLOCK_NUMBER } from '@/test/e2e/constants'
-// import { setupFork } from '@/test/e2e/forking/setupFork'
-// import { setup } from '@/test/e2e/setup'
-// import { test } from '@playwright/test'
-// import { mainnet } from 'viem/chains'
-// import { ConvertStablesDialogPageObject } from '../../ConvertStablesDialog.PageObject'
+import { SavingsPageObject } from '@/pages/Savings.PageObject'
+import { DEFAULT_BLOCK_NUMBER } from '@/test/e2e/constants'
+import { setup } from '@/test/e2e/setup'
+import { test } from '@playwright/test'
+import { mainnet } from 'viem/chains'
+import { ConvertStablesDialogPageObject } from '../../ConvertStablesDialog.PageObject'
 
-// test.describe('Convert DAI to USDC', () => {
-//   const fork = setupFork({ blockNumber: DEFAULT_BLOCK_NUMBER, chainId: mainnet.id, useTenderlyVnet: true })
-//   let savingsPage: SavingsPageObject
-//   let convertStablesDialog: ConvertStablesDialogPageObject
+test.describe('Convert DAI to USDC', () => {
+  let savingsPage: SavingsPageObject
+  let convertStablesDialog: ConvertStablesDialogPageObject
 
-//   test.beforeEach(async ({ page }) => {
-//     await setup(page, fork, {
-//       initialPage: 'savings',
-//       account: {
-//         type: 'connected-random',
-//         assetBalances: {
-//           ETH: 1,
-//           DAI: 10_000,
-//         },
-//       },
-//     })
+  test.beforeEach(async ({ page }) => {
+    const testContext = await setup(page, {
+      blockchain: {
+        blockNumber: DEFAULT_BLOCK_NUMBER,
+        chainId: mainnet.id,
+      },
+      initialPage: 'savings',
+      account: {
+        type: 'connected-random',
+        assetBalances: {
+          ETH: 1,
+          DAI: 10_000,
+        },
+      },
+    })
 
-//     savingsPage = new SavingsPageObject(page)
-//     await savingsPage.clickConvertStablesButtonAction()
+    savingsPage = new SavingsPageObject(testContext)
+    await savingsPage.clickConvertStablesButtonAction()
 
-//     convertStablesDialog = new ConvertStablesDialogPageObject(page)
-//     await convertStablesDialog.selectAssetInAction('DAI')
-//     await convertStablesDialog.selectAssetOutAction('USDC')
-//     await convertStablesDialog.fillAmountInAction(10_000)
-//   })
+    convertStablesDialog = new ConvertStablesDialogPageObject(testContext)
+    await convertStablesDialog.selectAssetInAction('DAI')
+    await convertStablesDialog.selectAssetOutAction('USDC')
+    await convertStablesDialog.fillAmountInAction(10_000)
+  })
 
-//   test('uses psm convert action', async () => {
-//     await convertStablesDialog.actionsContainer.expectEnabledActionAtIndex(0)
-//     await convertStablesDialog.actionsContainer.expectActions([
-//       { type: 'approve', asset: 'DAI' },
-//       { type: 'psmConvert', inToken: 'DAI', outToken: 'USDC' },
-//     ])
-//   })
+  test('uses psm convert action', async () => {
+    await convertStablesDialog.actionsContainer.expectEnabledActionAtIndex(0)
+    await convertStablesDialog.actionsContainer.expectActions([
+      { type: 'approve', asset: 'DAI' },
+      { type: 'psmConvert', inToken: 'DAI', outToken: 'USDC' },
+    ])
+  })
 
-//   test('displays transaction overview', async () => {
-//     await convertStablesDialog.expectTransactionOverview({
-//       routeItems: [
-//         {
-//           tokenAmount: '10,000.00 DAI',
-//           tokenUsdValue: '$10,000.00',
-//         },
-//         {
-//           tokenAmount: '10,000.00 USDC',
-//           tokenUsdValue: '$10,000.00',
-//         },
-//       ],
-//       outcome: '10,000.00 USDC',
-//       outcomeUsd: '$10,000.00',
-//     })
-//   })
+  test('displays transaction overview', async () => {
+    await convertStablesDialog.expectTransactionOverview({
+      routeItems: [
+        {
+          tokenAmount: '10,000.00 DAI',
+          tokenUsdValue: '$10,000.00',
+        },
+        {
+          tokenAmount: '10,000.00 USDC',
+          tokenUsdValue: '$10,000.00',
+        },
+      ],
+      outcome: '10,000.00 USDC',
+      outcomeUsd: '$10,000.00',
+    })
+  })
 
-//   test('executes conversion', async () => {
-//     await convertStablesDialog.actionsContainer.acceptAllActionsAction(2)
-//     await convertStablesDialog.expectSuccessPage()
-//     await convertStablesDialog.clickBackToSavingsButton()
-//     await savingsPage.expectStablecoinsInWalletAssetBalance('DAI', '-')
-//     await savingsPage.expectStablecoinsInWalletAssetBalance('USDC', '10,000.00')
-//   })
-// })
+  test('executes conversion', async () => {
+    await convertStablesDialog.actionsContainer.acceptAllActionsAction(2)
+    await convertStablesDialog.expectSuccessPage()
+    await convertStablesDialog.clickBackToSavingsButton()
+    await savingsPage.expectStablecoinsInWalletAssetBalance('DAI', '-')
+    await savingsPage.expectStablecoinsInWalletAssetBalance('USDC', '10,000.00')
+  })
+})
