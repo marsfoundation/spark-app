638c6a85d3ec8570f0a54f7b297e2ab2b71763d3
Oskar <43062492+oskarvu@users.noreply.github.com>
2025-01-14 15:39:05 +0700

    Increase sentry usefulness (#536)
    * Allow only spark specific domains in sentry
    
    * Filter out unwanted errors
    
    * Exclude sentry plugin from storybook build

--- packages/app/src/utils/sentry/index.ts
+++ packages/app/src/utils/sentry/index.ts
@@ -1,5 +1,6 @@
 import { apiUrl } from '@/config/consts'
 import * as Sentry from '@sentry/react'
+import { shouldErrorBeTracked } from './shouldErrorBeTracked'
 
 if (import.meta.env.VITE_SENTRY_DSN) {
   // biome-ignore lint/suspicious/noConsoleLog: <explanation>
@@ -10,17 +11,26 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   tunnel: `${apiUrl}/sentry/tunnel`,
   environment: import.meta.env.VITE_ENV_NAME,
-  integrations: [],
+  allowUrls: [
+    // main production domain
+    /^https:\/\/app\.spark\.fi/,
+    // vercel staging and production
+    /^https:\/\/spark-app-(staging|production)-mars-foundation\.vercel\.app/,
+    // localhost
+    ...(import.meta.env.VITE_SENTRY_DSN ? [/^https?:\/\/localhost:[0-9]+/] : []),
+  ],
+  integrations: [
+    // allows to filter out third party errors (browser extensions, code-injection, etc.)
+    Sentry.thirdPartyErrorFilterIntegration({
+      filterKeys: [import.meta.env.VITE_SENTRY_APPLICATION_KEY],
+      behaviour: 'drop-error-if-contains-third-party-frames',
+    }),
+  ],
   tracesSampleRate: 0,
   replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 0,
   tracePropagationTargets: [],
   maxValueLength: 5000,
-  ignoreErrors: [
-    'User rejected the request', // Rejecting a request using browser wallet
-    'User rejected methods', // Happens sometimes with mobile wallets
-    'User disapproved requested methods', // Happens when user rejects transaction using mobile wallet (connected by WalletConnect)
-  ],
   beforeSend(event, hint) {
     const error = hint.originalException
 
@@ -32,12 +42,6 @@ Sentry.init({
   },
   autoSessionTracking: false, // do not use sentry for analytics
 })
-
-function shouldErrorBeTracked(error: Error): boolean {
-  const trackedErrors = ['TypeError', 'AssertionError', 'ZodError']
-
-  return trackedErrors.some((errorName) => error.name === errorName)
-}
 
 export function captureError(error: Error): void {
   Sentry.captureException(error)
---
+++ packages/app/src/utils/sentry/shouldErrorBeTracked.ts
@@ -0,0 +1,36 @@
+export function shouldErrorBeTracked(error: Error): boolean {
+  if (isIgnoredMessage(error) || isIgnoredStackTrace(error)) {
+    return false
+  }
+  return isTrackedErrorType(error)
+}
+
+function isIgnoredMessage(error: Error): boolean {
+  const networkErrorMessages = [
+    'NetworkError',
+    'Failed to fetch',
+    'Load failed',
+    'Importing a module script failed',
+    'The I/O read operation failed',
+    'fetch failed',
+    'error loading dynamically imported module',
+    'Connection terminated unexpectedly',
+  ]
+  const walletUserInteractionErrors = [
+    'User rejected the request',
+    'User rejected methods',
+    'User disapproved requested methods',
+  ]
+  return [...networkErrorMessages, ...walletUserInteractionErrors].some((message) => error.message?.includes(message))
+}
+
+function isIgnoredStackTrace(error: Error): boolean {
+  const ignoredStackTraces = [/@wagmi\/core\/.*\/injected/, /@wagmi\/core\/.*\/disconnect/]
+  const errorStack = error.stack
+  return errorStack ? ignoredStackTraces.some((pattern) => pattern.test(errorStack)) : false
+}
+
+function isTrackedErrorType(error: Error): boolean {
+  const trackedErrors = ['TypeError', 'AssertionError', 'ZodError']
+  return trackedErrors.some((errorName) => error.name === errorName)
+}
