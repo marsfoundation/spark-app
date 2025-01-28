2dea541ef84d1b8c20bcd70cdc30b71ba90bd37f
oskarvu <oskar.vu.dev@gmail.com>
2025-01-28 16:19:14 +0700

    Put all errors in one array and add comments

--- packages/app/src/utils/sentry/shouldErrorBeTracked.ts
+++ packages/app/src/utils/sentry/shouldErrorBeTracked.ts
@@ -6,7 +6,8 @@ export function shouldErrorBeTracked(error: Error): boolean {
 }
 
 function isIgnoredMessage(error: Error): boolean {
-  const networkErrorMessages = [
+  const ignoredMessages = [
+    // Network errors
     'NetworkError',
     'Failed to fetch',
     'Load failed',
@@ -17,11 +18,12 @@ function isIgnoredMessage(error: Error): boolean {
     'Connection terminated unexpectedly',
     'Request expired',
     'The request took too long to respond',
-  ]
 
-  const unsupportedContractsAndMethods = ['does not support contract "ensUniversalResolver"', 'wallet_getCapabilities']
+    // Unsupported contracts and methods
+    'does not support contract "ensUniversalResolver"',
+    'wallet_getCapabilities',
 
-  const walletConnectorMessages = [
+    // Wallet connector errors
     'Connector not connected',
     'connector.getChainId is not a function',
     'provider.disconnect is not a function',
@@ -29,32 +31,26 @@ function isIgnoredMessage(error: Error): boolean {
     '.disconnect is not a function',
     'does not support programmatic chain switching',
     "does not match the connection's chain",
-  ]
 
-  const walletUserInteractionErrors = [
+    // Wallet user interaction errors
     'User rejected the request',
     'User rejected methods',
+    'Transaction was rejected',
     'User disapproved requested methods',
-  ]
+    'does not match the target chain for the transaction',
 
-  const allowanceErrors = [
+    // Allowance errors
     '"approve" reverted',
     'transfer amount exceeds allowance',
     'insufficient-allowance',
     'transfer-from-failed',
     'transfer from failed',
+
+    // Gas errors
+    'to pay the network fees',
   ]
 
-  const gasErrors = ['to pay the network fees']
-
-  return [
-    ...networkErrorMessages,
-    ...walletUserInteractionErrors,
-    ...unsupportedContractsAndMethods,
-    ...walletConnectorMessages,
-    ...allowanceErrors,
-    ...gasErrors,
-  ].some((message) => error.message?.includes(message))
+  return ignoredMessages.some((message) => error.message?.includes(message))
 }
 
 function isIgnoredStackTrace(error: Error): boolean {
