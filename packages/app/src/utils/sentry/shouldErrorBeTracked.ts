fe740b3a3c936507c2a0f7f7fd58498430bba2a2
Oskar <43062492+oskarvu@users.noreply.github.com>
2025-01-29 18:28:32 +0700

    Add more sentry error filters (#573)
    * Add more sentry error filters and adjust tack trace path matching
    
    * Put all errors in one array and add comments

--- packages/app/src/utils/sentry/shouldErrorBeTracked.ts
+++ packages/app/src/utils/sentry/shouldErrorBeTracked.ts
@@ -6,7 +6,8 @@ export function shouldErrorBeTracked(error: Error): boolean {
 }
 
 function isIgnoredMessage(error: Error): boolean {
-  const networkErrorMessages = [
+  const ignoredMessages = [
+    // Network errors
     'NetworkError',
     'Failed to fetch',
     'Load failed',
@@ -17,11 +18,12 @@ function isIgnoredMessage(error: Error): boolean {
     'Connection terminated unexpectedly',
     'Request expired',
     'The request took too long to respond',
-  ]
 
-  const unsupportedContractsAndMethods = ['does not support contract "ensUniversalResolver"', 'wallet_getCapabilities']
+    // Unsupported contracts and methods
+    'does not support contract "ensUniversalResolver"',
+    'wallet_getCapabilities',
 
-  const walletConnectorMessages = [
+    // Wallet connector errors
     'Connector not connected',
     'connector.getChainId is not a function',
     'provider.disconnect is not a function',
@@ -29,27 +31,30 @@ function isIgnoredMessage(error: Error): boolean {
     '.disconnect is not a function',
     'does not support programmatic chain switching',
     "does not match the connection's chain",
-  ]
 
-  const walletUserInteractionErrors = [
+    // Wallet user interaction errors
     'User rejected the request',
     'User rejected methods',
+    'Transaction was rejected',
     'User disapproved requested methods',
+    'does not match the target chain for the transaction',
+
+    // Allowance errors
+    '"approve" reverted',
+    'transfer amount exceeds allowance',
+    'insufficient-allowance',
+    'transfer-from-failed',
+    'transfer from failed',
+
+    // Gas errors
+    'to pay the network fees',
   ]
 
-  const allowanceErrors = ['"approve" reverted', 'transfer amount exceeds allowance', 'insufficient-allowance']
-
-  return [
-    ...networkErrorMessages,
-    ...walletUserInteractionErrors,
-    ...unsupportedContractsAndMethods,
-    ...walletConnectorMessages,
-    ...allowanceErrors,
-  ].some((message) => error.message?.includes(message))
+  return ignoredMessages.some((message) => error.message?.includes(message))
 }
 
 function isIgnoredStackTrace(error: Error): boolean {
-  const ignoredStackTraces = [/@wagmi\/core\/.*\/injected/, /@wagmi\/core\/.*\/disconnect/]
+  const ignoredStackTraces = [/@wagmi[\/\\]core.*[\/\\]injected/, /@wagmi[\/\\]core.*[\/\\]disconnect/]
   const errorStack = error.stack
   return errorStack ? ignoredStackTraces.some((pattern) => pattern.test(errorStack)) : false
 }
