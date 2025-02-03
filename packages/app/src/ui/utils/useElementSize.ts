18356d073f5a97b9b69a84da551c842bf03f4f0b
Oskar <43062492+oskarvu@users.noreply.github.com>
2025-02-03 19:23:57 +0700

    Fix chart width in savings view layout (#585)

--- packages/app/src/ui/utils/useElementSize.ts
+++ packages/app/src/ui/utils/useElementSize.ts
@@ -1,9 +1,9 @@
 import { RefObject, useRef } from 'react'
 import { useResizeObserver } from './useResizeObserver'
 
-type UseParentWidthReturnType<T> = [RefObject<T>, { width: number; height: number }]
+type UseElementSizeReturnType<T> = [RefObject<T>, { width: number; height: number }]
 
-export function useParentSize<T extends HTMLElement = HTMLDivElement>(): UseParentWidthReturnType<T> {
+export function useElementSize<T extends HTMLElement = HTMLDivElement>(): UseElementSizeReturnType<T> {
   const ref = useRef<T>(null)
 
   const size = useResizeObserver({
