4c103efc58fb266b3f5d81bbd50e7a8dbda71628
yivlad <jacenko.vlad@gmail.com>
2025-02-04 23:02:52 +0100

    Update

--- packages/app/src/ui/atoms/savings-accounts-top-banner/SavingsAccountsTopBanner.tsx
+++ packages/app/src/ui/atoms/savings-accounts-top-banner/SavingsAccountsTopBanner.tsx
@@ -1,9 +1,8 @@
 import { paths } from '@/config/paths'
 import { cn } from '@/ui/utils/style'
-import { XIcon } from 'lucide-react'
+import { ArrowUpRightIcon, XIcon } from 'lucide-react'
 import { IconButton } from '../icon-button/IconButton'
 import { LinkDecorator } from '../link-decorator/LinkDecorator'
-import { Sparkles } from '../sparkles/Sparkles'
 
 interface SavingsAccountsTopBannerProps {
   onClose: () => void
@@ -14,32 +13,31 @@ export const SAVINGS_ACCOUNTS_TOP_BANNER_ID = 'savings-accounts-top-banner'
 
 export function SavingsAccountsTopBanner({ onClose, className }: SavingsAccountsTopBannerProps) {
   return (
-    <div
-      className={cn(
-        'relative z-10 flex w-full flex-col items-center justify-center',
-        'gap-2 p-2.5 text-center shadow-inner sm:flex-row md:p-1.5',
-        'bg-[#FB5EAB]',
-        className,
-      )}
-    >
-      <div className="typography-label-3 md:typography-label-2 text-primary-inverse">
-        New!{'  '}
-        <br className="md:hidden" />
-        <Sparkles sizeRange={[12, 20]} className="mx-1 whitespace-nowrap">
-          <LinkDecorator to={paths.savings}>
-            <span className="text-[#FFCB4A]">Savings Accounts</span>
-          </LinkDecorator>
-        </Sparkles>
-        are here - start saving today.
-      </div>
+    <LinkDecorator to={paths.savings}>
+      <div
+        className={cn(
+          'relative z-10 flex w-full flex-col items-center justify-center',
+          'h-10 text-center shadow-inner sm:flex-row md:p-1.5',
+          'bg-[#5723F2] hover:bg-[#3a00a6]',
+          className,
+        )}
+      >
+        <div className="flex items-center gap-1 font-light text-primary-inverse">
+          <div>New! Savings Accounts are here - start saving today</div>
+          <ArrowUpRightIcon strokeWidth={1} />
+        </div>
 
-      <IconButton
-        icon={XIcon}
-        variant="transparent"
-        size="s"
-        onClick={onClose}
-        className="-translate-y-1/2 absolute top-1/2 right-3 text-primary-inverse hover:text-primary-inverse/70 active:text-primary-inverse/50"
-      />
-    </div>
+        <IconButton
+          icon={XIcon}
+          variant="transparent"
+          size="s"
+          onClick={(e) => {
+            e.stopPropagation()
+            onClose()
+          }}
+          className="-translate-y-1/2 absolute top-1/2 right-3 text-primary-inverse hover:text-primary-inverse/70 active:text-primary-inverse/50"
+        />
+      </div>
+    </LinkDecorator>
   )
 }
