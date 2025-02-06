7781e00becf8ff8da868d58ddd7c47fe478f284d
oskarvu <oskar.vu.dev@gmail.com>
2025-02-06 17:01:39 +0700

    Wrap in use effect just to be sure

--- packages/app/src/features/savings/logic/general-stats/usePrefetchSavingsGeneralStats.ts
+++ packages/app/src/features/savings/logic/general-stats/usePrefetchSavingsGeneralStats.ts
@@ -1,7 +1,10 @@
 import { useQueryClient } from '@tanstack/react-query'
+import { useEffect } from 'react'
 import { generalStatsQueryOptions } from './generalStatsQueryOptions'
 
 export function usePrefetchSavingsGeneralStats(): void {
   const queryClient = useQueryClient()
-  void queryClient.prefetchQuery(generalStatsQueryOptions())
+  useEffect(() => {
+    void queryClient.prefetchQuery(generalStatsQueryOptions())
+  }, [queryClient])
 }
