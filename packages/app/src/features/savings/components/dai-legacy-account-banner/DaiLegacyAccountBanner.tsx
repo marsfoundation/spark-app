22654b0fe7568db7152b47dcc086861a52f21568
oskarvu <oskar.vu.dev@gmail.com>
2025-02-13 18:50:21 +0700

    Improve legacy banner text flow

--- packages/app/src/features/savings/components/dai-legacy-account-banner/DaiLegacyAccountBanner.tsx
+++ packages/app/src/features/savings/components/dai-legacy-account-banner/DaiLegacyAccountBanner.tsx
@@ -23,19 +23,17 @@ export function DaiLegacyAccountBanner({ apyImprovement }: UpgradeSavingsBannerP
           'grid w-full grid-rows-[1fr_auto] items-center gap-2 sm:gap-4 lg:grid-cols-[1fr_auto] lg:grid-rows-1 lg:gap-6',
         )}
       >
-        <div className="flex max-w-[380px] flex-col gap-1">
+        <div className="flex flex-col gap-1">
           <div className="typography-label-2 sm:typography-heading-5 text-primary">
-            This is a legacy Savings Account.
-            <br />
-            You can deposit your DAI to Savings USDS
+            This is a legacy Savings Account. <br className="hidden sm:inline" />
+            You can deposit your DAI to Savings USDS <br className="hidden sm:inline" />
             {apyImprovement ? (
               <>
-                {' '}
                 and get <span className="text-system-success-primary">{formatPercentage(apyImprovement)}</span> higher
                 APY!
               </>
             ) : (
-              <> and unlock the full potential of Spark Savings.</>
+              <>and unlock the full potential of Spark Savings.</>
             )}
           </div>
         </div>
