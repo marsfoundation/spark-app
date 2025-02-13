06ced529d68a3ff2befbca39458586b099559121
yivlad <jacenko.vlad@gmail.com>
2025-02-13 17:15:29 +0100

    Revert "Polyfil buffer to support mobile coinbase wallet connection"
    This reverts commit 6c4c8efe17f47d29a15ea27e9fc1443acd079c68.

--- packages/app/src/config/wagmi/getWallets.ts
+++ packages/app/src/config/wagmi/getWallets.ts
@@ -1,15 +1,8 @@
-// biome-ignore lint/style/useNodejsImportProtocol:
-import { Buffer } from 'buffer'
 import { assert } from '@marsfoundation/common-universal'
 import { WalletList, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { okxWallet, safeWallet } from '@rainbow-me/rainbowkit/wallets'
 
 export function getWallets(): WalletList {
-  // @note: polyfill Buffer for browser to support coinbase wallet mobile connection
-  if (!window.Buffer) {
-    window.Buffer = Buffer
-  }
-
   const popularWalletsGroup = getDefaultWallets().wallets[0]
   assert(popularWalletsGroup, 'Popular wallets group should be defined')
   const { groupName, wallets: popularWallets } = popularWalletsGroup
