01dea4207890d0bc323c8dde25c36ec8184606bb
oskarvu <oskar.vu.dev@gmail.com>
2025-02-13 22:34:20 +0700

    Polyfil buffer to support mobile coinbase wallet connection

--- packages/app/src/config/wagmi/getWallets.ts
+++ packages/app/src/config/wagmi/getWallets.ts
@@ -1,8 +1,14 @@
 import { assert } from '@marsfoundation/common-universal'
 import { WalletList, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { okxWallet, safeWallet } from '@rainbow-me/rainbowkit/wallets'
+import { Buffer } from 'buffer'
 
 export function getWallets(): WalletList {
+  // @note: polyfill Buffer for browser to support coinbase wallet mobile connection
+  if (!window.Buffer) {
+    window.Buffer = Buffer
+  }
+
   const popularWalletsGroup = getDefaultWallets().wallets[0]
   assert(popularWalletsGroup, 'Popular wallets group should be defined')
   const { groupName, wallets: popularWallets } = popularWalletsGroup
