b709bfde0d7bc4666c5602a0d39ba854530c8f11
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2025-02-24 09:38:46 +0100

    Add useActiveRewards hook (#653)

--- packages/app/src/config/consts.ts
+++ packages/app/src/config/consts.ts
@@ -11,6 +11,7 @@ export const ZUSTAND_APP_STORE_LOCAL_STORAGE_VERSION = 2
 export const apiUrl = import.meta.env.VITE_API_URL ?? '/api'
 export const blockAnaliticaApiUrl = import.meta.env.VITE_BLOCK_ANALITICA_API_URL ?? '/ba-api'
 export const infoSkyApiUrl = import.meta.env.VITE_INFO_SKY_API_URL ?? '/info-sky-api'
+export const spark2ApiUrl = import.meta.env.VITE_SPARK2_API_URL ?? '/spark2-api'
 
 // @note: all sandboxes created by the app begin (when expressed as strings) with this chain id. Ex: 30301719211032
 export const SANDBOX_NETWORKS_CHAIN_ID_PREFIX = 3030
