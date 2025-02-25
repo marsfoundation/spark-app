3f21107f26d51b0b042d98f59ca7f00badf0226c
Krzysztof Kaczor <krzkaczor@gmail.com>
2025-02-25 16:34:59 +0100

    Tweak getRandomChainId to work with any chain ids

--- packages/common-testnets/src/helpers/getRandomChainId.ts
+++ packages/common-testnets/src/helpers/getRandomChainId.ts
@@ -1,6 +1,12 @@
 /**
- * Use to generate a chainId with low probability of colliding with real network
+ * Generate a semi-random chainId with a prefix of 7357 (test) so it shouldn't collide with any real network.
+ * Having unique chainIds makes DX better when adding custom chains to wallets.
  */
-export function getRandomChainId(originChainId: number): number {
-  return Number.parseInt(`${originChainId}3030${Date.now()}`)
+export function getRandomChainId(): number {
+  const uniquePostfix = Math.floor(Math.random() * longestSafePostfix)
+  const paddedPostfix = uniquePostfix.toString().padStart(longestSafePostfix.toString().length, '0')
+
+  return Number.parseInt(`7357${paddedPostfix}`)
 }
+
+const longestSafePostfix = 99_999 // final chainId should be less than 2^32-1, so 7357 prefix leaves us with 5 digits for the rest
