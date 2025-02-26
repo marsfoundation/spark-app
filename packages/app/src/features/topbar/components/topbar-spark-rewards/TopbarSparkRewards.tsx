d41232ec4bdc83b7f44fbb0dbaa441a76f0ca7de
yivlad <jacenko.vlad@gmail.com>
2025-02-27 00:17:15 +0100

    Add spark rewards navigation element

---
+++ packages/app/src/features/topbar/components/topbar-spark-rewards/TopbarSparkRewards.tsx
@@ -0,0 +1,33 @@
+import { paths } from '@/config/paths'
+import { USD_MOCK_TOKEN } from '@/domain/types/Token'
+import { assets } from '@/ui/assets'
+import { cn } from '@/ui/utils/style'
+import { NormalizedUnitNumber } from '@marsfoundation/common-universal'
+import { forwardRef } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export interface TopbarSparkRewardsProps {
+  totalUsdAmount?: NormalizedUnitNumber
+}
+
+export const TopbarSparkRewards = forwardRef<HTMLAnchorElement | HTMLButtonElement, TopbarSparkRewardsProps>(
+  ({ totalUsdAmount }, ref) => {
+    return (
+      <NavLink to={paths.sparkRewards} ref={ref as React.ForwardedRef<HTMLAnchorElement>}>
+        <div
+          className={cn(
+            'relative isolate flex w-fit items-center gap-2 rounded-sm border p-2',
+            'before:absolute before:inset-[-1px] before:z-[-2] before:rounded-[9px] before:bg-gradient-spark-secondary',
+            'after:absolute after:inset-0 after:z-[-1] after:rounded-sm after:bg-primary',
+            'focus-visible:after:hidden focus-visible:before:hidden',
+          )}
+        >
+          <img src={assets.giftGradient} className="size-6" />
+          {totalUsdAmount && (
+            <span className="typography-button-1 text-primary">{USD_MOCK_TOKEN.formatUSD(totalUsdAmount)}</span>
+          )}
+        </div>
+      </NavLink>
+    )
+  },
+)
