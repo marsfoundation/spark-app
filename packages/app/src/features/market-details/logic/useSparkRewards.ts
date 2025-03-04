faaf15321c9b51b71eeb433988116a4a164b0637
oskarvu <oskar.vu.dev@gmail.com>
2025-03-04 14:53:11 +0700

    Improve import

--- packages/app/src/features/market-details/logic/useSparkRewards.ts
+++ packages/app/src/features/market-details/logic/useSparkRewards.ts
@@ -1,9 +1,9 @@
 import { Reserve } from '@/domain/market-info/marketInfo'
 import { assignSparkRewards } from '@/domain/spark-rewards/assignSparkRewards'
 import { ongoingCampaignsQueryOptions } from '@/domain/spark-rewards/ongoingCampaignsQueryOptions'
+import { SparkReward } from '@/domain/spark-rewards/types'
 import { useQuery } from '@tanstack/react-query'
 import { useConfig } from 'wagmi'
-import { SparkReward } from '../../../domain/spark-rewards/types'
 
 export interface UseSparkRewardsParams {
   chainId: number
