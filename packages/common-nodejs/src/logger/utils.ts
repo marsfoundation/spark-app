3d3626ba82e0e480de3f4a6e19d480e390bf0c14
Bartłomiej Tarczyński <bartlomiej.tarczynski1@gmail.com>
2025-03-05 16:17:21 +0100

    Remove unused formatDate function

--- packages/common-nodejs/src/logger/utils.ts
+++ packages/common-nodejs/src/logger/utils.ts
@@ -18,8 +18,3 @@ export function toJSON(parameters: object): string {
     return v
   })
 }
-
-export function formatDate(date: Date): string {
-  const padStart = (value: number): string => value.toString().padStart(2, '0')
-  return `${padStart(date.getDate())}-${padStart(date.getMonth() + 1)}-${date.getFullYear()}`
-}
