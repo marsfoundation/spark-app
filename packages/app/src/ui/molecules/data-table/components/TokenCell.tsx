6227fc4a4bd7e4fe3b1f42a0745d49e17add7c7e
oskarvu <oskar.vu.dev@gmail.com>
2025-03-06 13:36:35 +0700

    Use icon stack in token cell

--- packages/app/src/ui/molecules/data-table/components/TokenCell.tsx
+++ packages/app/src/ui/molecules/data-table/components/TokenCell.tsx
@@ -1,5 +1,5 @@
 import { Token } from '@/domain/types/Token'
-import { TokenIcon } from '@/ui/atoms/token-icon/TokenIcon'
+import { IconStack } from '../../icon-stack/IconStack'
 
 export interface TokenCellProps {
   token: Token
@@ -8,7 +8,7 @@ export interface TokenCellProps {
 export function TokenCell({ token }: TokenCellProps) {
   return (
     <div className="typography-label-2 flex flex-row items-center gap-2">
-      <TokenIcon token={token} className="icon-md" />
+      <IconStack items={[token]} />
       {token.symbol}
     </div>
   )
