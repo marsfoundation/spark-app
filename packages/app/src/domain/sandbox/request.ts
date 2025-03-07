45ef21c854848c6411f93d858f63314570812517
Bartłomiej Tarczyński <bartlomiej.tarczynski1@gmail.com>
2025-03-07 14:01:49 +0100

    Replace usages of solidFetch

--- packages/app/src/domain/sandbox/request.ts
+++ packages/app/src/domain/sandbox/request.ts
@@ -1,5 +1,7 @@
 import { randomInt } from '@/utils/random'
-import { solidFetch } from '@marsfoundation/common-universal'
+import { HttpClient } from '@marsfoundation/common-universal/http-client'
+import { Logger } from '@marsfoundation/common-universal/logger'
+import { z } from 'zod'
 
 export async function request(
   forkUrl: string,
@@ -11,28 +13,18 @@ export async function request(
   method: 'tenderly_setErc20Balance',
   params: [string, string, string],
 ): Promise<unknown>
-export async function request(forkUrl: string, method: 'evm_snapshot', params: []): Promise<unknown>
-export async function request(forkUrl: string, method: 'evm_revert', params: [string]): Promise<unknown>
-export async function request(forkUrl: string, method: 'evm_increaseTime', params: [number]): Promise<unknown>
-export async function request(forkUrl: string, method: 'evm_setNextBlockTimestamp', params: [number]): Promise<unknown>
 export async function request(forkUrl: string, method: string, params: any[]): Promise<unknown> {
   const id = randomInt().toString()
+  const httpClient = new HttpClient(Logger.BROWSER)
 
-  const result = await solidFetch(forkUrl, {
-    method: 'post',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({
+  return httpClient.post(
+    forkUrl,
+    {
       jsonrpc: '2.0',
       method,
       params,
       id,
-    }),
-  })
-
-  if (!result.ok) {
-    throw new Error(`${method} failed: ${await result.text()}`)
-  }
-
-  const data = await result.json()
-  return data
+    },
+    z.unknown(),
+  )
 }
