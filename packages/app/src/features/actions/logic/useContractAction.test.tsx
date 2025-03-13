3fbc7f145be6109a43d2ad9b7f7752fa8171f4ba
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2025-03-13 11:29:49 +0100

    Use setupUseContractActionRenderer for useContractAction test (#722)

--- packages/app/src/features/actions/logic/useContractAction.test.tsx
+++ packages/app/src/features/actions/logic/useContractAction.test.tsx
@@ -1,60 +1,46 @@
 import { aaveContractErrors } from '@/config/aaveContractErrors'
 import { poolAbi } from '@/config/abis/poolAbi'
 import { lendingPoolConfig } from '@/config/contracts-generated'
-import { TestingWrapper } from '@/test/integration/TestingWrapper'
 import { getMockMarketInfo, testAddresses, testTokens } from '@/test/integration/constants'
-import { handlers, makeMockTransport } from '@/test/integration/mockTransport'
-import { queryClient as defaultQueryClient } from '@/test/integration/query-client'
-import { createWagmiTestConfig } from '@/test/integration/wagmi-config'
+import { handlers } from '@/test/integration/mockTransport'
+import { setupUseContractActionRenderer } from '@/test/integration/setupUseContractActionRenderer'
 import { NormalizedUnitNumber, toBigInt } from '@marsfoundation/common-universal'
-import { renderHook, waitFor } from '@testing-library/react'
+import { waitFor } from '@testing-library/react'
 import { mainnet } from 'viem/chains'
 import { test } from 'vitest'
 import { useContractAction } from './useContractAction'
 
+const userAddress = testAddresses.alice
+const chainId = mainnet.id
+const token = testTokens.WETH
+const value = NormalizedUnitNumber(1)
+
+const hookRenderer = setupUseContractActionRenderer({
+  account: userAddress,
+  handlers: [handlers.chainIdCall({ chainId })],
+  args: {
+    action: { type: 'withdraw', token, value },
+    enabled: true,
+    context: {
+      marketInfo: getMockMarketInfo(),
+    },
+  },
+})
+
 describe(useContractAction.name, () => {
   test('translates aave errors', async () => {
-    const userAddress = testAddresses.alice
-    const chainId = mainnet.id
-    const token = testTokens.WETH
-    const amount = NormalizedUnitNumber(1)
-
-    const wagmiConfig = createWagmiTestConfig({
-      transport: makeMockTransport([
+    const { result } = hookRenderer({
+      extraHandlers: [
         handlers.contractCallError({
           abi: poolAbi,
           to: lendingPoolConfig.address[chainId],
           from: userAddress,
           functionName: 'withdraw',
-          args: [token.address, toBigInt(token.toBaseUnit(amount)), userAddress],
+          args: [token.address, toBigInt(token.toBaseUnit(value)), userAddress],
           errorMessage: '57',
         }),
-      ]),
-      wallet: { address: userAddress },
-      chain: mainnet,
-    })
-
-    const { result } = renderHook(useContractAction, {
-      initialProps: {
-        context: {
-          account: userAddress,
-          chainId,
-          txReceipts: [],
-          wagmiConfig,
-          marketInfo: getMockMarketInfo(),
-        },
-        action: {
-          type: 'withdraw',
-          token,
-          value: amount,
-        },
-        enabled: true,
-      },
-      wrapper: ({ children }) => (
-        <TestingWrapper config={wagmiConfig} queryClient={defaultQueryClient}>
-          {children}
-        </TestingWrapper>
-      ),
+        handlers.mineTransaction(),
+      ],
     })
 
     await waitFor(() => expect((result.current.state as any).message).toBe(aaveContractErrors['57']))
