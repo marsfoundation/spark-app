d3030a7d91d4caaa8568b1a52ec744c227c2804c
yivlad <jacenko.vlad@gmail.com>
2025-03-14 14:24:02 +0100

    Add E2E tets for claim functionality

---
+++ packages/app/src/features/dialogs/claim-spark-rewards/ClaimSparkRewardsDialog.test-e2e.ts
@@ -0,0 +1,150 @@
+import { TestContext, setup } from '@/test/e2e/setup'
+import { test } from '@playwright/test'
+import { mainnet } from 'viem/chains'
+
+import { SparkRewardsPageObject } from '@/pages/SparkRewards.PageObject'
+import { NormalizedUnitNumber } from '@marsfoundation/common-universal'
+import { ClaimSparkRewardsDialogPageObject } from './ClaimSparkRewardsDialog.PageObject'
+
+test.describe('Spark Rewards', () => {
+  test.describe('One reward', () => {
+    let sparkRewardsPage: SparkRewardsPageObject
+    let testContext: TestContext<'connected-random'>
+
+    test.beforeEach(async ({ page }) => {
+      testContext = await setup(page, {
+        blockchain: { blockNumber: 21926420n, chain: mainnet },
+        initialPage: 'sparkRewards',
+        account: {
+          type: 'connected-random',
+          sparkRewards: [
+            {
+              rewardTokenSymbol: 'USDS',
+              cumulativeAmount: NormalizedUnitNumber(101),
+            },
+          ],
+        },
+      })
+
+      sparkRewardsPage = new SparkRewardsPageObject(testContext)
+    })
+
+    test('can claim', async () => {
+      await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+      await sparkRewardsPage.clickClaimButton(0)
+
+      const claimDialog = new ClaimSparkRewardsDialogPageObject(testContext)
+
+      await claimDialog.actionsContainer.acceptAllActionsAction(1)
+      await claimDialog.clickCloseButtonAction()
+      await sparkRewardsPage.expectAmountToClaim(0, '0.00')
+    })
+
+    test('claim all claims one', async () => {
+      await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+      await sparkRewardsPage.clickClaimAllButton()
+
+      const claimDialog = new ClaimSparkRewardsDialogPageObject(testContext)
+
+      await claimDialog.actionsContainer.acceptAllActionsAction(1)
+      await claimDialog.clickCloseButtonAction()
+      await sparkRewardsPage.expectAmountToClaim(0, '0.00')
+    })
+  })
+
+  test.describe('Multiple rewards', () => {
+    test.describe('No wallet batching', () => {
+      let sparkRewardsPage: SparkRewardsPageObject
+      let testContext: TestContext<'connected-random'>
+
+      test.beforeEach(async ({ page }) => {
+        testContext = await setup(page, {
+          blockchain: { blockNumber: 21926420n, chain: mainnet },
+          initialPage: 'sparkRewards',
+          account: {
+            type: 'connected-random',
+            sparkRewards: [
+              {
+                rewardTokenSymbol: 'USDS',
+                cumulativeAmount: NormalizedUnitNumber(101),
+              },
+              {
+                rewardTokenSymbol: 'USDC',
+                cumulativeAmount: NormalizedUnitNumber(202),
+              },
+            ],
+          },
+        })
+
+        sparkRewardsPage = new SparkRewardsPageObject(testContext)
+      })
+
+      test('can claim one reward', async () => {
+        await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '202.00')
+        await sparkRewardsPage.clickClaimButton(1)
+
+        const claimDialog = new ClaimSparkRewardsDialogPageObject(testContext)
+
+        await claimDialog.actionsContainer.acceptAllActionsAction(1)
+        await claimDialog.clickCloseButtonAction()
+        await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '0.00')
+      })
+
+      test('can claim all', async () => {
+        await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '202.00')
+        await sparkRewardsPage.clickClaimAllButton()
+
+        const claimDialog = new ClaimSparkRewardsDialogPageObject(testContext)
+
+        await claimDialog.actionsContainer.acceptAllActionsAction(2)
+        await claimDialog.clickCloseButtonAction()
+        await sparkRewardsPage.expectAmountToClaim(0, '0.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '0.00')
+      })
+    })
+
+    test.describe('Wallet batching', () => {
+      let sparkRewardsPage: SparkRewardsPageObject
+      let testContext: TestContext<'connected-random'>
+
+      test.beforeEach(async ({ page }) => {
+        testContext = await setup(page, {
+          blockchain: { blockNumber: 21926420n, chain: mainnet },
+          initialPage: 'sparkRewards',
+          account: {
+            type: 'connected-random',
+            atomicBatchSupported: true,
+            sparkRewards: [
+              {
+                rewardTokenSymbol: 'USDS',
+                cumulativeAmount: NormalizedUnitNumber(101),
+              },
+              {
+                rewardTokenSymbol: 'USDC',
+                cumulativeAmount: NormalizedUnitNumber(202),
+              },
+            ],
+          },
+        })
+
+        sparkRewardsPage = new SparkRewardsPageObject(testContext)
+      })
+
+      test('can claim all', async () => {
+        await sparkRewardsPage.expectAmountToClaim(0, '101.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '202.00')
+        await sparkRewardsPage.clickClaimAllButton()
+
+        const claimDialog = new ClaimSparkRewardsDialogPageObject(testContext)
+
+        await claimDialog.actionsContainer.acceptBatchedActions()
+        await claimDialog.clickCloseButtonAction()
+        await sparkRewardsPage.expectAmountToClaim(0, '0.00')
+        await sparkRewardsPage.expectAmountToClaim(1, '0.00')
+      })
+    })
+  })
+})
--- packages/app/src/pages/SparkRewards.PageObject.ts
+++ packages/app/src/pages/SparkRewards.PageObject.ts
@@ -1,18 +1,30 @@
-import { expect } from '@playwright/test'
+import { Locator, expect } from '@playwright/test'
 
 import { BasePageObject } from '@/test/e2e/BasePageObject'
 import { testIds } from '@/ui/utils/testIds'
 
 export class SparkRewardsPageObject extends BasePageObject {
+  // #region locators
+  locateClaimableRewardsRow(index: number): Locator {
+    return this.locatePanelByHeader('Claimable rewards').getByTestId(testIds.component.DataTable.row(index))
+  }
+  // #endregion
+
   // #region actions
-  async clickClaimButton(): Promise<void> {
-    await this.page.getByRole('button', { name: 'Claim' }).first().click()
+  async clickClaimButton(index: number): Promise<void> {
+    await this.locateClaimableRewardsRow(index).getByRole('button', { name: 'Claim' }).click()
+  }
+
+  async clickClaimAllButton(): Promise<void> {
+    await this.page.getByRole('button', { name: 'Claim all' }).click()
   }
   // #endregion
 
   // #region assertions
-  async expectAmountToClaim(amount: string): Promise<void> {
-    await expect(this.page.getByTestId(testIds.sparkRewards.claimableRewardsPanel.amountToClaim)).toHaveText(amount)
+  async expectAmountToClaim(rewardIndex: number, amount: string): Promise<void> {
+    await expect(
+      this.locateClaimableRewardsRow(rewardIndex).getByTestId(testIds.sparkRewards.claimableRewardsPanel.amountToClaim),
+    ).toHaveText(amount)
   }
   // #endregion
 }
